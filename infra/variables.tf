b12c9c6d83a27db011504fd0a55bbbd5831e0d04
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-07-11 20:10:36 +0200

    Add terraform configuration for cloud run

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,22 +1,41 @@
-variable "repository" {
+variable "google_region" {
   type        = string
-  description = "The repository to pull the image from"
-  default     = "ghcr.io/input-output-hk"
+  description = "The region on GCP"
+  default     = "us-east1"
 }
 
-variable "image" {
+variable "google_zone" {
   type        = string
-  description = "The image to pull"
-  default     = "ouroboros-leios"
+  description = "The zone on GCP"
+  default     = "us-east1-b"
 }
 
-variable "tag" {
+variable "google_service_account" {
   type        = string
-  description = "The tag to pull"
-  default     = "latest"
+  description = "The service account to use for deploying services"
 }
 
-variable "ghcr-pat" {
+variable "google_service_credentials_json_file" {
   type        = string
-  description = "The Personal Access Token to use to pull the image"
+  description = "The credentials of the GCP service account"
+}
+
+variable "terraform_backend_bucket" {
+  type = string
+  description = "The bucket to use to store terraform's state"
+  default = "peras_infra"
+}
+
+variable "terraform_backend_prefix" {
+  type = string
+  description = "The prefix path to store terraform's state at"
+  default = "terraform/peras/state"
+}
+
+locals {
+  google_service_credentials_json_file_decoded = jsondecode(file(var.google_service_credentials_json_file))
+  google_service_account_private_key           = local.google_service_credentials_json_file_decoded.private_key
+  google_project_id                            = local.google_service_credentials_json_file_decoded.project_id
+  terraform_backend_prefix = var.terraform_backend_prefix
+  terraform_backend_bucket = var.terraform_backend_bucket
 }
