4057bdbe9ba72936b7efebba4d1ad0d8986b77f4
Andrea Vezzosi <andrea@well-typed.com>
2024-10-04 17:22:48 +0200

    simulation: stubbed chainSyncProducer impl

--- simulation/src/PraosProtocol.hs
+++ simulation/src/PraosProtocol.hs
@@ -1,10 +1,33 @@
+{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module PraosProtocol where
 
+import Control.Concurrent.Class.MonadSTM (
+  MonadSTM (
+    STM,
+    TMVar,
+    TVar,
+    atomically,
+    modifyTVar',
+    newTVarIO,
+    readTVar,
+    readTVarIO,
+    retry,
+    takeTMVar,
+    tryTakeTMVar,
+    writeTVar
+  ),
+ )
+import Control.Monad (when)
 import Network.TypedProtocol
+import Network.TypedProtocol.Peer.Server as TS
 
 --------------------------------
 --- Common types
@@ -14,9 +37,9 @@ import Network.TypedProtocol
 data SlotNo
 data BlockId
 data BlockHeader
-data ChainTip
+type ChainTip = Point
 
-data Point = Point BlockId SlotNo
+data Point = Point BlockId SlotNo -- could be just the slot?
 
 --------------------------------
 ---- ChainSync
@@ -36,26 +59,32 @@ data SingChainSyncState (st :: ChainSyncState) where
   SingStIntersect :: SingChainSyncState StIntersect
   SingStDone :: SingChainSyncState StDone
 
+data StateCanRoll (st :: ChainSyncState) where
+  IsCanAwait :: StateCanRoll StCanAwait
+  IsMustReply :: StateCanRoll StMustReply
+
 instance Protocol ChainSyncState where
   data Message ChainSyncState from to where
-    MsgRequestNext ::
+    MsgRequestNext :: Message ChainSyncState StIdle StCanAwait
+    MsgAwaitReply :: Message ChainSyncState StCanAwait StMustReply
+    MsgRollForward ::
+      StateCanRoll st ->
       BlockHeader ->
       ChainTip ->
-      Message ChainSyncState StIdle StCanAwait
-    MsgAwaitReply :: Message ChainSyncState StCanAwait StMustReply
-    MsgRollForward :: Message ChainSyncState StCanAwait StIdle
+      Message ChainSyncState st StIdle
     MsgRollBackward ::
+      StateCanRoll st ->
       Point ->
       ChainTip ->
-      Message ChainSyncState StCanAwait StIdle
+      Message ChainSyncState st StIdle
     MsgFindIntersect ::
       [Point] ->
       Message ChainSyncState StIdle StIntersect
-    MsgIntersectNotFound ::
-      ChainTip ->
-      Message ChainSyncState StIntersect StIdle
     MsgIntersectFound ::
       Point ->
+      ChainTip ->
+      Message ChainSyncState StIntersect StIdle
+    MsgIntersectNotFound ::
       ChainTip ->
       Message ChainSyncState StIntersect StIdle
     MsgDone :: Message ChainSyncState StIdle StDone
@@ -71,3 +100,61 @@ instance StateTokenI StCanAwait where stateToken = SingStCanAwait
 instance StateTokenI StMustReply where stateToken = SingStMustReply
 instance StateTokenI StIntersect where stateToken = SingStIntersect
 instance StateTokenI StDone where stateToken = SingStDone
+
+-- Where the consumer is at, in reading our chain.
+
+data ReadPointer = ReadPointer {point :: Point, isBackward :: Bool}
+type MonadSyncProducer m = MonadSTM m -- TODO
+
+data NodeEvent = NewBlock | ChainSwitch
+
+instance Semigroup NodeEvent where
+  _ <> ChainSwitch = ChainSwitch
+  ChainSwitch <> _ = ChainSwitch
+  NewBlock <> NewBlock = NewBlock
+
+data ChainSyncProducerState m = ChainSyncProducerState
+  { readPointer :: TVar m ReadPointer
+  , ownChainTip :: TVar m ChainTip -- TODO: Read-only. Make newtype
+  , mailbox :: TMVar m NodeEvent
+  }
+
+onPar :: Point -> ChainTip -> Bool
+onPar = undefined
+
+nextPointAndHeader :: MonadSyncProducer m => ChainSyncProducerState m -> Point -> STM m (Point, BlockHeader)
+nextPointAndHeader = undefined
+
+checkMail :: MonadSyncProducer m => ChainSyncProducerState m -> Bool -> STM m ()
+checkMail = undefined
+
+chainSyncProducer ::
+  forall m.
+  MonadSyncProducer m =>
+  ChainSyncProducerState m ->
+  TS.Server ChainSyncState NonPipelined StIdle m ()
+chainSyncProducer st@ChainSyncProducerState{..} = idle
+ where
+  idle :: TS.Server ChainSyncState NonPipelined StIdle m ()
+  idle = TS.Await $ \case
+    MsgDone -> TS.Done ()
+    MsgRequestNext -> TS.Effect $ atomically $ do
+      checkMail st False
+      rp <- readTVar readPointer
+      tip <- readTVar ownChainTip
+      if
+          | point rp `onPar` tip -> do
+              when (isBackward rp) $ writeTVar readPointer rp{isBackward = False}
+              return $ TS.Yield MsgAwaitReply mustReply
+          | isBackward rp -> do
+              writeTVar readPointer rp{isBackward = False}
+              return $ TS.Yield (MsgRollBackward IsCanAwait (point rp) tip) idle
+          | otherwise -> do
+              (nextPoint, header) <- nextPointAndHeader st (point rp)
+              writeTVar readPointer $ ReadPointer nextPoint False
+              return $ TS.Yield (MsgRollForward IsCanAwait header tip) idle
+    MsgFindIntersect points -> undefined
+  mustReply :: TS.Server ChainSyncState 'NonPipelined 'StMustReply m ()
+  mustReply = TS.Effect $ atomically $ do
+    checkMail st True
+    undefined -- TODO: like MsgRequestNext
