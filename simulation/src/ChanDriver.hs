6cba628dfd518cdc1a42d0b6fb2be12e2feede51
Andrea Vezzosi <andrea@well-typed.com>
2024-10-18 16:04:55 +0200

    completed ChanDriver

--- simulation/src/ChanDriver.hs
+++ simulation/src/ChanDriver.hs
@@ -1,12 +1,47 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RankNTypes #-}
 
 module ChanDriver where
 
 import Chan
 import Network.TypedProtocol
+import ChanTCP
+import Data.Type.Equality
 
 data ProtocolMessage ps where
-  ProtocolMessage :: SomeMessage st -> ProtocolMessage ps
+  ProtocolMessage :: forall ps (st :: ps). SomeMessage st -> ProtocolMessage ps
 
-chanDriver :: Chan m (ProtocolMessage ps) -> Driver ps pr () m
-chanDriver _ch = undefined
+instance Show (ProtocolMessage ps) where
+  show _ = "TODO"
+
+instance forall ps. (forall st st'. MessageSize (Message ps st st')) => MessageSize (ProtocolMessage ps) where
+  messageSizeBytes (ProtocolMessage (SomeMessage msg)) = messageSizeBytes msg
+
+type CompareStateToken ps = forall (st :: ps) (st' :: ps). (StateTokenI st, StateTokenI st') => Maybe (st :~: st')
+
+
+chanDriver :: forall ps pr m. Monad m => CompareStateToken ps -> Chan m (ProtocolMessage ps) -> Driver ps pr () m
+chanDriver cmp ch = Driver{sendMessage = \ _ msg -> writeChan ch (ProtocolMessage (SomeMessage msg))
+                      , recvMessage = recvMessage
+                      , initialDState = ()
+                      }
+  where
+    recvMessage :: forall (st :: ps).
+                           (StateTokenI st, ActiveState st) =>
+                           TheyHaveAgencyProof pr st -> () -> m (SomeMessage st, ())
+    recvMessage _ _ = do
+      ProtocolMessage smsg <- readChan ch
+      case smsg of
+        SomeMessage @_ @st1 @st' msg -> case cmp @st @st1 of
+          Just Refl -> pure (SomeMessage msg, ())
+          Nothing -> error "TODO"
