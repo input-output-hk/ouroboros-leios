427b74906aec36bf0a564fe44579281514d86a7b
Wen Kokke <wenkokke@users.noreply.github.com>
2024-10-21 13:44:48 +0100

    impl SimBlockFetch

--- simulation/src/ChanDriver.hs
+++ simulation/src/ChanDriver.hs
@@ -15,7 +15,15 @@ module ChanDriver where
 import Chan
 import ChanTCP
 import Data.Type.Equality
-import Network.TypedProtocol
+import Network.TypedProtocol (
+  ActiveState,
+  Driver (..),
+  Protocol (Message),
+  SomeMessage (..),
+  StateTokenI,
+  TheyHaveAgencyProof,
+  WeHaveAgencyProof,
+ )
 
 data ProtocolMessage ps where
   ProtocolMessage :: forall ps (st :: ps). SomeMessage st -> ProtocolMessage ps
@@ -28,23 +36,37 @@ instance forall ps. (forall st st'. MessageSize (Message ps st st')) => MessageS
 
 type CompareStateToken ps = forall (st :: ps) (st' :: ps). (StateTokenI st, StateTokenI st') => Maybe (st :~: st')
 
-chanDriver :: forall ps pr m. Monad m => CompareStateToken ps -> Chan m (ProtocolMessage ps) -> Driver ps pr () m
+chanDriver ::
+  forall ps pr m.
+  Monad m =>
+  CompareStateToken ps ->
+  Chan m (ProtocolMessage ps) ->
+  Driver ps pr () m
 chanDriver cmp ch =
   Driver
-    { sendMessage = \_ msg -> writeChan ch (ProtocolMessage (SomeMessage msg))
+    { sendMessage = sendMessage
     , recvMessage = recvMessage
     , initialDState = ()
     }
  where
+  sendMessage ::
+    forall (st :: ps) (st' :: ps).
+    (StateTokenI st, StateTokenI st', ActiveState st) =>
+    WeHaveAgencyProof pr st ->
+    Message ps st st' ->
+    m ()
+  sendMessage _prf msg =
+    writeChan ch (ProtocolMessage (SomeMessage msg))
+
   recvMessage ::
     forall (st :: ps).
     (StateTokenI st, ActiveState st) =>
     TheyHaveAgencyProof pr st ->
     () ->
     m (SomeMessage st, ())
-  recvMessage _ _ = do
+  recvMessage _prf () = do
     ProtocolMessage smsg <- readChan ch
     case smsg of
-      SomeMessage @_ @st1 @st' msg -> case cmp @st @st1 of
+      SomeMessage @_ @st' msg -> case cmp @st @st' of
         Just Refl -> pure (SomeMessage msg, ())
         Nothing -> error "TODO"
