01c701896c649508653af76691f3e4308edff43a
Andrea Vezzosi <andrea@well-typed.com>
2024-12-19 15:33:00 +0100

    simulation: integrated bounded worker pool into leiosNode

--- simulation/src/WorkerPool.hs
+++ simulation/src/WorkerPool.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE GADTSyntax #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTSyntax #-}
+
 module WorkerPool where
 
--- | A simple simulation of a worker pool with either bounded or unbounded
+-- \| A simple simulation of a worker pool with either bounded or unbounded
 -- CPU resources.
 --
 -- The intention is to use this where we need to model CPU resources being
@@ -19,36 +20,43 @@ module WorkerPool where
 -- work and execute it. Thus there are at most N tasks executing concurrently
 -- and the total task CPU throughput is N * time. In this approach, the order
 -- of the sources is relevant: when there are multiple tasks available, new
--- tasks are 
+-- tasks are
 
 import Control.Concurrent.Class.MonadSTM
+import Control.Monad
 import Control.Monad.Class.MonadAsync
 import Control.Monad.Class.MonadThrow
-import Control.Monad
 
 data Task m where
   Task :: m a -> !(TMVar m (Either SomeException a)) -> Task m
 
 type TaskSource m = STM m (Task m)
 
-newBoundedWorkerPool :: (MonadAsync m, MonadCatch m, MonadSTM m)
-                     => Int -> [TaskSource m] -> m ()
+newBoundedWorkerPool ::
+  (MonadAsync m, MonadCatch m, MonadSTM m) =>
+  Int ->
+  [TaskSource m] ->
+  m ()
 newBoundedWorkerPool n sources =
-    replicateConcurrentlyOn_ n worker
-  where
-    worker = forever $ do
-      Task task resultVar <- atomically $ foldr orElse retry sources
-      result <- try task
-      atomically $ writeTMVar resultVar result
+  replicateConcurrentlyOn_ n worker
+ where
+  worker = forever $ do
+    Task task resultVar <- atomically $ foldr orElse retry sources
+    result <- try task
+    atomically $ writeTMVar resultVar result
 
 replicateConcurrentlyOn_ :: (MonadThrow m, MonadAsync m) => Int -> m () -> m ()
 replicateConcurrentlyOn_ n action =
-    void $
-      bracket
-        (forM [0..n-1] $ \i -> asyncOn i action)
-        (mapM_ cancel) --TODO prefer cancelMany
-        waitAny
+  void $
+    bracket
+      (forM [0 .. n - 1] $ \i -> asyncOn i action)
+      cancelMany
+      waitAny
 
-newUnboundedWorkerPool :: MonadSTM m => [STM m (m ())] -> m ()
-newUnboundedWorkerPool _sources = undefined
+-- newUnboundedWorkerPool :: MonadSTM m => [STM m (m ())] -> m ()
+-- newUnboundedWorkerPool _sources = undefined
 
+cancelMany :: MonadAsync m => [Async m a] -> m ()
+cancelMany as = do
+  mapM_ cancel as
+  mapM_ waitCatch as
