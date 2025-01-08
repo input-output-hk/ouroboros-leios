8b24903f787d577bc3bc0006e3b284a24fca482e
Yves Hauser <yves.hauser@iohk.io>
2025-01-08 11:15:48 +0100

    Added TODOs

--- leios-conformance/src/Leios/Conformance/Model.hs
+++ leios-conformance/src/Leios/Conformance/Model.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Leios.Conformance.Model where
 
 import Data.Aeson (FromJSON, ToJSON)
+import Data.Maybe (maybeToList)
 import GHC.Generics (Generic)
 import Text.PrettyPrint.HughesPJClass (Pretty (pPrint))
-
-type Chain = String
-type Vote = String
 
 data EnvAction
   = Tick
@@ -15,6 +14,7 @@ data EnvAction
   | NewEB EndorserBlock
   deriving (Eq, Show)
 
+-- TODO: use InputBlock extracted from Agda
 data InputBlock = InputBlock
   deriving (Show, Eq, Generic)
 
@@ -23,6 +23,7 @@ instance ToJSON InputBlock
 instance Pretty InputBlock where
   pPrint InputBlock = "InputBlock"
 
+-- TODO: use EndorserBlock extracted from Agda
 data EndorserBlock = EndorserBlock
   deriving (Show, Eq, Generic)
 
@@ -31,7 +32,7 @@ instance ToJSON EndorserBlock
 instance Pretty EndorserBlock where
   pPrint EndorserBlock = "EndorserBlock"
 
--- TODO: LeiosState
+-- TODO: use LeiosState extracted from Agda
 data NodeModel = NodeModel
   { slot :: Integer
   , ibs :: [InputBlock]
@@ -42,8 +43,30 @@ data NodeModel = NodeModel
 initialModelState :: NodeModel
 initialModelState = NodeModel 0 [] []
 
+--TODO
+makeIB :: NodeModel -> Maybe InputBlock
+makeIB _ = Nothing
+
+--TODO
+makeEB :: NodeModel -> Maybe EndorserBlock
+makeEB _ = Nothing
+
+addIB :: InputBlock -> NodeModel -> NodeModel
+addIB ib nm@NodeModel{..} = nm { ibs = ib : ibs }
+
+addEB :: EndorserBlock -> NodeModel -> NodeModel
+addEB eb nm@NodeModel{..} = nm { ebs = eb : ebs }
+
 -- TODO: Leios executable specification
 transition :: NodeModel -> EnvAction -> Maybe (([InputBlock], [EndorserBlock]), NodeModel)
-transition nm Tick = Just (([], []), nm)
-transition nm (NewIB _) = Just (([], []), nm)
-transition nm (NewEB _) = Just (([], []), nm)
+transition nm Tick = do
+  -- TODO: guards
+  let ibs = maybeToList (makeIB nm)
+      ebs = maybeToList (makeEB nm)
+  pure ((ibs, ebs), nm)
+transition nm (NewIB ib) = do
+  -- TODO: guards
+  pure (([], []), addIB ib nm)
+transition nm (NewEB eb) = do
+  -- TODO: guards
+  pure (([], []), addEB eb nm)
