3b9fe02c34d0855e57fe249dab3a2360fc843f92
Brian W Bush <brian.bush@iohk.io>
2025-01-14 10:54:49 -0700

    Benchmarked voter sortition.

--- leios-crypto-benchmarks/src/main.rs
+++ leios-crypto-benchmarks/src/main.rs
@@ -4,6 +4,7 @@ use rustc_apfloat::ieee::Quad;
 
 
 fn main() {
+
     let f : Quad = "4.8771764574959465286614323309274434524463393558834E-2".parse::<Quad>().unwrap();
     let f1 : Quad = ln_1_minus(f);
     let s : Quad = into_quad(0.001);
@@ -14,4 +15,14 @@ fn main() {
     println!("1 - (1 -f)^s = {} vs {} ", p, leader_value(f1, s));
     println!("Lower: {}", leader_check(f1, s, p0));
     println!("Upper: {}", leader_check(f1, s, p1));
+
+    let votes : Quad = into_quad(500.0);
+    let mut pv : Quad = into_quad(0.0);
+    loop {
+      pv = (pv + into_quad(0.01)).value;
+      if pv > into_quad(1.0) {
+        break;
+      }
+      println!("{} {}", pv, voter_check(votes, s, pv))
+    }
 }
