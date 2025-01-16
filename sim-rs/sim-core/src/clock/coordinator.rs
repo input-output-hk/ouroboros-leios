92198608ec1d728f7a5da1c7fa54edfbb7f6f53d
Simon Gellis <simongellis@gmail.com>
2025-01-16 17:26:55 -0500

    sim-rs: fix race condition in clock, add tests

--- sim-rs/sim-core/src/clock/coordinator.rs
+++ sim-rs/sim-core/src/clock/coordinator.rs
@@ -56,7 +56,7 @@ impl ClockCoordinator {
         while let Some(event) = self.rx.recv().await {
             match event {
                 ClockEvent::Wait { actor, until, done } => {
-                    if waiters[actor].replace(done).is_some() {
+                    if waiters[actor].replace(Waiter { until, done }).is_some() {
                         panic!("An actor has somehow managed to wait twice");
                     }
                     running -= 1;
@@ -70,8 +70,10 @@ impl ClockCoordinator {
                             let Some(waiter) = waiters[id].take() else {
                                 continue;
                             };
-                            running += 1;
-                            let _ = waiter.send(());
+                            if waiter.until == timestamp {
+                                running += 1;
+                                let _ = waiter.done.send(());
+                            }
                         }
                     }
                 }
@@ -91,6 +93,11 @@ impl ClockCoordinator {
     }
 }
 
+struct Waiter {
+    until: Timestamp,
+    done: oneshot::Sender<()>,
+}
+
 #[derive(Debug)]
 pub enum ClockEvent {
     Wait {
@@ -103,4 +110,109 @@ pub enum ClockEvent {
     },
     StartTask,
     FinishTask,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{task::Poll, time::Duration};
+
+    use futures::poll;
+    use tokio::pin;
+
+    use super::ClockCoordinator;
+
+    #[tokio::test]
+    async fn should_wait_once_all_workers_are_waiting() {
+        let mut coordinator = ClockCoordinator::new();
+        let clock = coordinator.clock();
+        let t0 = clock.now();
+        let t1 = t0 + Duration::from_millis(5);
+        let t2 = t0 + Duration::from_millis(10);
+        let mut actor1 = clock.barrier();
+        let mut actor2 = clock.barrier();
+
+        let run_future = coordinator.run();
+        pin!(run_future);
+
+        let wait1 = actor1.wait_until(t1);
+        pin!(wait1);
+        assert_eq!(poll!(&mut wait1), Poll::Pending); // the wait is pending
+        assert_eq!(poll!(&mut run_future), Poll::Pending); // try advancing time
+        assert_eq!(clock.now(), t0); // no time has passed
+        assert_eq!(poll!(&mut wait1), Poll::Pending); // the 5ms wait is still pending, because clock 2 isn't finished
+
+        let wait2 = actor2.wait_until(t2);
+        pin!(wait2);
+        assert_eq!(poll!(&mut wait2), Poll::Pending);
+        assert_eq!(poll!(&mut run_future), Poll::Pending); // try advancing time
+        assert_eq!(clock.now(), t1); // 5ms have passed
+        assert_eq!(poll!(&mut wait2), Poll::Pending); // the 10ms wait is still pending
+        assert_eq!(poll!(wait1), Poll::Ready(())); // the 5ms wait is done
+    }
+
+    #[tokio::test]
+    async fn should_cancel_wait_when_wait_future_is_dropped() {
+        let mut coordinator = ClockCoordinator::new();
+        let clock = coordinator.clock();
+        let t0 = clock.now();
+        let t1 = t0 + Duration::from_millis(5);
+        let mut actor1 = clock.barrier();
+        let mut actor2 = clock.barrier();
+
+        let run_future = coordinator.run();
+        pin!(run_future);
+
+        {
+            let wait1 = actor1.wait_until(t1);
+            pin!(wait1);
+            assert_eq!(poll!(wait1), Poll::Pending); // the wait is pending
+            // and now it goes out of scope and gets dropped
+        }
+        assert_eq!(poll!(&mut run_future), Poll::Pending); // try advancing time
+        assert_eq!(poll!(&mut run_future), Poll::Pending); // try advancing time
+        assert_eq!(clock.now(), t0); // no time has passed
+
+        let wait2 = actor2.wait_until(t1);
+        pin!(wait2);
+        assert_eq!(poll!(&mut wait2), Poll::Pending);
+        assert_eq!(poll!(&mut run_future), Poll::Pending); // try advancing time
+        assert_eq!(clock.now(), t0); // no time has passed
+        assert_eq!(poll!(&mut wait2), Poll::Pending); // the remaining wait is still pending
+    }
+
+    #[tokio::test]
+    async fn should_avoid_race_condition() {
+        let mut coordinator = ClockCoordinator::new();
+        let clock = coordinator.clock();
+        let t0 = clock.now();
+        let t1 = t0 + Duration::from_millis(5);
+        let t2 = t0 + Duration::from_millis(10);
+        let mut actor1 = clock.barrier();
+        let mut actor2 = clock.barrier();
+
+        let run_future = coordinator.run();
+        pin!(run_future);
+
+        // make actor 1 wait for a short time, then cancel it, then wait for a long time
+        {
+            let wait1 = actor1.wait_until(t1);
+            pin!(wait1);
+            assert_eq!(poll!(wait1), Poll::Pending);
+        }
+        let wait1 = actor1.wait_until(t2);
+        pin!(wait1);
+        assert_eq!(poll!(&mut wait1), Poll::Pending);
+        assert_eq!(poll!(&mut run_future), Poll::Pending);
+        assert_eq!(clock.now(), t0); // no time has passed
+        assert_eq!(poll!(&mut wait1), Poll::Pending);
+
+        let wait2 = actor2.wait_until(t2);
+        pin!(wait2);
+        assert_eq!(poll!(wait2), Poll::Pending);
+        while let Poll::Pending = poll!(&mut wait1) {
+            assert_eq!(poll!(&mut run_future), Poll::Pending);
+        }
+        // We expect a long time to have passed, because the "short" wait was cancelled
+        assert_eq!(clock.now(), t2);
+    }
 }
