c924a4c63fc12b2761adec04832c108354abeca5
will-break-it <will-break-it@users.noreply.github.com>
2025-01-27 14:11:52 +0000

    deploy: f47e6025b18eeab96deb9e0e709d9f21d63ef5ff

---
+++ assets/js/48387199.3ec00d6a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[248],{6655:e=>{e.exports=JSON.parse('{"tags":[{"label":"progress","permalink":"/news/tags/progress","count":16},{"label":"update","permalink":"/news/tags/update","count":16},{"label":"weekly","permalink":"/news/tags/weekly","count":16},{"label":"education","permalink":"/news/tags/education","count":1}]}')}}]);
