cf644ce8b295c5caff59f410bbe9a444e43687fc
Brian W Bush <brian.bush@iohk.io>
2025-02-03 10:30:12 -0700

    Made message sizes more realistic

--- crypto-benchmarks.rs/benches/vote_bench.rs
+++ crypto-benchmarks.rs/benches/vote_bench.rs
@@ -12,7 +12,7 @@ fn benchmark_check_pop(c: &mut Criterion) {
 fn benchmark_gen_vote(c: &mut Criterion) {
     let sk: SecretKey = gen_key();
     let eid = b"Election ID";
-    let m: [u8; 500] = [0; 500];
+    let m: [u8; 64] = [0; 64];
     c.bench_function("GenVote", |b| b.iter(|| gen_vote(&sk, eid, &m)));
 }
 
@@ -20,26 +20,26 @@ fn benchmark_verify_vote(c: &mut Criterion) {
     let sk: SecretKey = gen_key();
     let pk: blst::min_sig::PublicKey = sk.sk_to_pk();
     let eid = b"Election ID";
-    let m: [u8; 500] = [0; 500];
+    let m: [u8; 64] = [0; 64];
     let vs: VoteSignature = gen_vote(&sk, eid, &m);
     c.bench_function("VerifyVote", |b| b.iter(|| verify_vote(&pk, eid, &m, &vs)));
 }
 
 fn benchmark_gen_cert(c: &mut Criterion) {
-    let sks: Vec<SecretKey> = (0..300).map(|_| gen_key()).collect();
+    let sks: Vec<SecretKey> = (0..750).map(|_| gen_key()).collect();
     let eid = b"Election ID";
-    let m: [u8; 500] = [0; 500];
+    let m: [u8; 64] = [0; 64];
     let vss: Vec<VoteSignature> = sks.iter().map(|sk| gen_vote(&sk, eid, &m)).collect();
     let vs_refs: Vec<&VoteSignature> = vss.iter().map(|vs| vs).collect();
     c.bench_function("GenCert", |b| b.iter(|| gen_cert(&vs_refs)));
 }
 
 fn benchmark_verify_cert(c: &mut Criterion) {
-    let sks: Vec<SecretKey> = (0..300).map(|_| gen_key()).collect();
+    let sks: Vec<SecretKey> = (0..750).map(|_| gen_key()).collect();
     let pks: Vec<PublicKey> = sks.iter().map(|sk| sk.sk_to_pk()).collect();
     let pk_refs: Vec<&PublicKey> = pks.iter().map(|pk| pk).collect();
     let eid = b"Election ID";
-    let m: [u8; 500] = [0; 500];
+    let m: [u8; 64] = [0; 64];
     let vss: Vec<VoteSignature> = sks.iter().map(|sk| gen_vote(&sk, eid, &m)).collect();
     let vs_refs: Vec<&VoteSignature> = vss.iter().map(|vs| vs).collect();
     let cs: CertSignature = gen_cert(&vs_refs).unwrap();
