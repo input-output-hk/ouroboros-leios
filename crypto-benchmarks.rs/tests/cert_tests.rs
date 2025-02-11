4ce4573468446f7cc8aefa39e7e3905818297131
Brian W Bush <brian.bush@iohk.io>
2025-02-11 12:46:46 -0700

    Added test cases for certificates

--- crypto-benchmarks.rs/tests/cert_tests.rs
+++ crypto-benchmarks.rs/tests/cert_tests.rs
@@ -1,5 +1,43 @@
 #[macro_use(quickcheck)]
 extern crate quickcheck_macros;
 
-//#[quickcheck]
-//fn prop_verif_cert()
+use leios_crypto_benchmarks::cert::*;
+use leios_crypto_benchmarks::registry::*;
+use leios_crypto_benchmarks::vote::*;
+use quickcheck::Arbitrary;
+
+#[derive(Clone, Debug)]
+struct CertScenario {
+  pub reg: Registry,
+  pub votes: Vec<Vote>,
+}
+
+
+impl Arbitrary for CertScenario {
+  fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+      let reg = Registry::arbitrary(g);
+      let votes = arbitrary_votes(g, &reg);
+      CertScenario {reg, votes}
+  }
+}
+
+#[quickcheck]
+fn prop_gen_cert(scenario: CertScenario) -> bool {
+  gen_cert(&scenario.reg, &scenario.votes).is_some()
+}
+
+#[quickcheck]
+fn prop_verify_cert(scenario: CertScenario) -> bool {
+  match gen_cert(&scenario.reg, &scenario.votes) {
+    Some(cert) => {verify_cert(&scenario.reg, &cert)}
+    None => {false}
+  }
+}
+
+#[quickcheck]
+fn prop_weigh_cert(scenario: CertScenario) -> bool {
+  match gen_cert(&scenario.reg, &scenario.votes) {
+    Some(cert) => {weigh_cert(&scenario.reg, &cert).is_some()}
+    None => {false}
+  }
+}
