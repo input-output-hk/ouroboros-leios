0d13cd15da1c964ef4c854d1953c1acd8d0ddac6
Andrea Vezzosi <andrea@well-typed.com>
2025-03-26 22:40:08 +0100

    simulation: moved configuration and topology to leios-trace-hs

--- simulation/src/SimTypes.hs
+++ simulation/src/SimTypes.hs
@@ -11,17 +11,20 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ViewPatterns #-}
 
-module SimTypes where
+module SimTypes (
+  module LeiosTypes,
+  module SimTypes,
+) where
 
-import Data.Aeson.Types (FromJSON (..), FromJSONKey, KeyValue ((.=)), Parser, ToJSON (..), ToJSONKey, Value (..), object, typeMismatch, withObject, (.!=), (.:), (.:?))
+import Data.Aeson.Types (FromJSON (..), FromJSONKey, KeyValue ((.=)), ToJSON (..), ToJSONKey, Value (..), object, typeMismatch, withObject, (.!=), (.:), (.:?))
 import Data.Default (Default (..))
 import Data.Hashable
 import Data.Ix (Ix)
 import Data.Text (Text)
 import qualified Data.Text as T
-import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import GHC.Records
+import LeiosTypes
 import TimeCompat
 
 data CPUTask = CPUTask {cpuTaskDuration :: !DiffTime, cpuTaskLabel :: !Text}
@@ -41,27 +44,6 @@ newtype NodeId = NodeId Int
 data LabelNode e = LabelNode NodeId e deriving (Show)
 
 data LabelLink e = LabelLink NodeId NodeId e deriving (Show)
-
--- | Position in simulation world coordinates
-data Point = Point {_1 :: !Double, _2 :: !Double}
-  deriving (Eq, Show, Generic)
-
-instance ToJSON Point where
-  toJSON :: Point -> Value
-  toJSON (Point x y) = toJSON [x, y]
-
-instance FromJSON Point where
-  parseJSON :: Value -> Parser Point
-  parseJSON = withTuple "Point" $ \(x, y) ->
-    Point <$> parseJSON x <*> parseJSON y
-   where
-    withTuple :: String -> ((Value, Value) -> Parser a) -> Value -> Parser a
-    withTuple _expected k (toTuple -> Just xy) = k xy
-    withTuple expected _k v = typeMismatch expected v
-
-    toTuple :: Value -> Maybe (Value, Value)
-    toTuple (Array v) | V.length v == 2 = Just (v V.! 0, v V.! 1)
-    toTuple _ = Nothing
 
 -- | Path in simulation world
 newtype Path = Path [Point]
