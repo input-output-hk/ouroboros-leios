a8c5d47dc118e13ed1560c0c27114546747bb178
Brian W Bush <brian.bush@iohk.io>
2025-04-10 05:01:07 -0600

    Added lookups to queries

--- analysis/sims/2025w15/queries/rust/rbgen.sh
+++ analysis/sims/2025w15/queries/rust/rbgen.sh
@@ -4,12 +4,14 @@
 jq -c '
   select(.message.type == "RBGenerated")
 | {
-    "time": .time_s,
     "rb": .message.id,
+    "time": .time_s,
+    "size": "NA",
     "eb-count": (if .message.endorsement == null then 0 else 1 end)
   }
 ' "$1" \
 | sort \
 | dasel -r json -w csv \
 | sed -n -e '1p;0~2p' \
+| gzip -9c \
 > "$2"
