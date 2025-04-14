6462667a81c260f71c7b6bc3c8e550f03b65c7a5
Brian W Bush <brian.bush@iohk.io>
2025-04-14 08:27:53 -0600

    Expanded experiment

--- analysis/sims/2025w16/build-experiments.sh
+++ analysis/sims/2025w16/build-experiments.sh
@@ -12,11 +12,11 @@ do
     do
       for IB_RATE in 0.5 1.0 2.0 4.0 8.0 16.0
       do
-        for IB_SIZE in 98304
+        for IB_SIZE in 65536 131072 
         do
-          for EB_RATE in 1.5
+          for EB_RATE in 1.0 1.5 2.0
           do
-            for STAGE_LENGTH in 20
+            for STAGE_LENGTH in 10 20 40
             do
               ./make-experiment.sh $SIMULATOR $MAX_SLOT $LABEL $NETWORK $IB_RATE $IB_SIZE $EB_RATE $STAGE_LENGTH
             done
