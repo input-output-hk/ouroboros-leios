5a197fbd2ab0b00997567d5beadc18a4c26946db
Brian W Bush <brian.bush@iohk.io>
2025-04-14 08:00:06 -0600

    Revised Haskell script for cpu analysis to handle file-format changes

---
+++ analysis/sims/2025w16/queries/haskell/cpus.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p jq dasel gnused gzip
+
+jq -c '
+  select(.message.type == "Cpu")
+| {
+    "idx": {
+      "slot": (.time_s | floor),
+      "node": .message.node,
+      "task": .message.task_type
+    },
+    "duration": .message.cpu_time_s
+  }
+' "$1" \
+| sort \
+| jq -sc '
+  group_by(.idx)
+| map(.[0].idx + {"duration": (map(.duration) | add)})
+| .[]
+' \
+| dasel -r json -w csv \
+| sed -n -e '1p;0~2p' \
+| gzip -9c \
+> "$2"
