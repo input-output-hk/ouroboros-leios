b6f01633cf8e9be70d5e92f1b8ab7d6daa121198
Brian W Bush <brian.bush@iohk.io>
2025-04-14 08:13:56 -0600

    Designed experiment for tag `leios-2025w16`

---
+++ analysis/sims/2025w16/build-experiments.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -ev
+
+MAX_SLOT=600
+
+for SIMULATOR in rust haskell
+do
+  for LABEL in default
+  do
+    for NETWORK in 100-nodes
+    do
+      for IB_RATE in 0.5 1.0 2.0 4.0 8.0 16.0
+      do
+        for IB_SIZE in 98304
+        do
+          for EB_RATE in 1.5
+          do
+            for STAGE_LENGTH in 20
+            do
+              ./make-experiment.sh $SIMULATOR $MAX_SLOT $LABEL $NETWORK $IB_RATE $IB_SIZE $EB_RATE $STAGE_LENGTH
+            done
+          done
+        done
+      done
+    done
+  done
+done
