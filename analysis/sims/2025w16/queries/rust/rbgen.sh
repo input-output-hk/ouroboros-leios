912dafc26c48146695dba406ad2f84a4b88d44a3
Brian W Bush <brian.bush@iohk.io>
2025-04-14 07:34:13 -0600

    Revised Rust script for RB analysis to handle file-format changes

---
+++ analysis/sims/2025w16/queries/rust/rbgen.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p jq dasel gnused gzip
+
+jq -c '
+  select(.message.type == "RBGenerated")
+| {
+    "rb": .message.id,
+    "time": .time_s,
+    "size": .message.size_bytes,
+    "eb-count": (if .message.endorsement == null then 0 else 1 end)
+  }
+' "$1" \
+| sort \
+| dasel -r json -w csv \
+| sed -n -e '1p;0~2p' \
+| gzip -9c \
+> "$2"
