a51233195fd0072d45e2a42fe90ca8cf8a1bf7b2
Brian W Bush <brian.bush@iohk.io>
2025-04-15 13:11:03 -0600

    Scripts for individual scenarios

---
+++ analysis/sims/2025w16/runs/rust-default-100-nodes-8.0-65536-1.0-20/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+../../sim-cli --parameters config.json network.yaml --slots 600 sim.log > stdout 2> stderr
+
+for f in ../../queries/*.sh
+do
+  n=$(basename ${f%%.sh})
+  $f sim.log $n.csv.gz
+done
+gzip -9f sim.log
+
+echo "FINISHED: rust | 600 | default | 100-nodes | 8.0 | 65536 | 1.0 | 20"
