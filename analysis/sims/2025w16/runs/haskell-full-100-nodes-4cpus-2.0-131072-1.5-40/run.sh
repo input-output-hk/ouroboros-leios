17bc59dc97ce6636d3fb832d93aca1d7315b68ae
Brian W Bush <bbush@ip-172-31-50-249.us-west-2.compute.internal>
2025-04-17 11:46:47 +0000

    Script files for full-leios runs

---
+++ analysis/sims/2025w16/runs/haskell-full-100-nodes-4cpus-2.0-131072-1.5-40/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+../../ols sim short-leios --leios-config-file config.json --topology-file network.yaml --shared-log-format JSON --output-file sim.log --output-seconds 600 > stdout 2> stderr
+
+for f in ../../queries/*.sh
+do
+  n=$(basename ${f%%.sh})
+  $f sim.log $n.csv.gz
+done
+gzip -9f sim.log
+
+echo "FINISHED: haskell | 600 | full | 100-nodes-4cpus | 2.0 | 131072 | 1.5 | 40"
