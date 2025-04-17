6ed7eb068142944952c438299ec8799bc23c2505
Simon Gellis <simongellis@gmail.com>
2025-04-17 14:27:22 -0400

    Include visualizer in docs site

--- site/sidebars.ts
+++ site/sidebars.ts
@@ -34,6 +34,11 @@ const sidebars: SidebarsConfig = {
         "development/throughput-simulation",
         "development/simulation-demonstration",
         "development/cost-estimator",
+        {
+          type: "link",
+          href: "/visualizer",
+          label: "Visualizer",
+        },
       ],
     },
   ],
