5f6575c2d1ed6d7288af8617f9c9a28800e27127
Brian W Bush <brian.bush@iohk.io>
2025-05-12 13:17:01 -0600

    Fixed nix derivation for `ols`.

--- nix/project.nix
+++ nix/project.nix
@@ -8,18 +8,28 @@ let
     name = "leios-hs-sources";
     src = ./..;
     patchPhase = ''
+      # Add the trace verifier package.
       sed -i '/^packages:/a\ \ leios-trace-verifier/dist/haskell' cabal.project
+      # Clean up troublesome symbolic links.
+      rm -r simulation/test/data
+      cp -r data simulation/test/
     '';
     buildPhase = ''
+      # Copy the source for the trace verifier.
       mkdir -p $out/leios-trace-verifier/dist/haskell
-      cp -r . $out
       cp -r ${agda.hsTraceParser.out}/hs-src/* $out/leios-trace-verifier/dist/haskell/
+      # Copy the original source.
+      cp -r . $out
     '';
     installPhase = ''
+      # Add the MAlonzo modules to the cabal file.
       chmod +w $out/leios-trace-verifier/dist/haskell/trace-parser.cabal
       find $out/leios-trace-verifier/dist/haskell/src/MAlonzo -name "*.hs" -print\
       | sed "s#^.*/src/#        #;s#\.hs##;s#/#.#g" \
       >> $out/leios-trace-verifier/dist/haskell/trace-parser.cabal
+    '';
+    fixupPhase = ''
+      # Skip fixup phase, so as not to mangle any of the source.
     '';
   };
 
@@ -30,7 +40,7 @@ let
       isCross = pkgs.stdenv.hostPlatform != pkgs.stdenv.buildPlatform;
     in
     {
-      src = sources;
+      src = sources.out;
       shell.withHoogle = false;
       inputMap = {
         "https://chap.intersectmbo.org/" = inputs.iogx.inputs.CHaP;
