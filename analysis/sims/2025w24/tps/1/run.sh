ff0e1fc2232a2bf9d0bb394eea87c99bc0be84d6
Brian W Bush <brian.bush@iohk.io>
2025-06-12 09:27:15 -0600

    Mini-mainnet experiments at tag `leios-2025w24`

---
+++ analysis/sims/2025w24/tps/1/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+mkfifo sim.log
+
+grep -E -v '(Slot|No.*Generated|CpuTask|Sent|Lottery)' sim.log | xz -9c > sim.log.xz &
+
+../../sim-cli --parameters config.yaml network.yaml --slots 720 --conformance-events sim.log > stdout 2> stderr
+
+wait
+rm sim.log
+
+cat << EOI > case.csv
+simulator,tps
+rust,$(basename $PWD)
+EOI
+
+cat case.csv
