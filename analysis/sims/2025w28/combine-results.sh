cd940ae33de94bf4ae56e767a1dce65299b2f41b
Brian W Bush <brian.bush@iohk.io>
2025-07-08 05:42:30 -0600

    Completed 1000 TPS with no IBs

---
+++ analysis/sims/2025w28/combine-results.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p gzip
+
+set -e
+
+for d in quick
+do
+  mkdir -p results/$d
+  for f in cpus lifecycle resources receipts
+  do
+    DIR=$(find $d -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n -quit)
+    HL=$(sed -n -e '1p' "$DIR/case.csv")
+    HR=$(zcat "$DIR/$f.csv.gz" | sed -n -e '1p')
+    (
+      echo "$HL,$HR"
+      for g in $(find $d -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n)
+      do
+        if [ ! -e "$g/stderr" ]
+        then
+          echo "Skipping $g because it has no stderr." >> /dev/stderr
+        elif [ -s "$g/stderr" ]
+        then
+          echo "Skipping $g because its stderr is not empty." >> /dev/stderr
+        else
+          BL=$(sed -n -e '2p' "$g/case.csv")
+          zcat "$g/$f.csv.gz" | sed -e "1d;s/^/$BL,/;s/null/NA/g"
+        fi
+      done
+    ) | pigz -p 3 -9c > results/$d/$f.csv.gz &
+  done
+done
+wait
