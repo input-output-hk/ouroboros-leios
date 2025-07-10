0ba6566bfa81675ed72dc57251f93f377e2ef6b6
Simon Gellis <simongellis@gmail.com>
2025-07-10 10:40:27 -0400

    sim-rs: add TXs to linear leios

--- sim-rs/sim-core/src/sim/linear_leios.rs
+++ sim-rs/sim-core/src/sim/linear_leios.rs
@@ -1,11 +1,10 @@
-#![expect(unused)] // TODO: get rid of this
-use std::{sync::Arc, time::Duration};
+use std::{collections::HashMap, sync::Arc, time::Duration};
 
 use rand_chacha::ChaChaRng;
 
 use crate::{
     clock::Clock,
-    config::{CpuTimeConfig, NodeConfiguration, NodeId, SimConfiguration},
+    config::{CpuTimeConfig, NodeConfiguration, NodeId, RelayStrategy, SimConfiguration},
     events::EventTracker,
     model::{Transaction, TransactionId},
     sim::{MiniProtocol, NodeImpl, SimCpuTask, SimMessage},
@@ -62,8 +61,18 @@ impl SimCpuTask for CpuTask {
     }
 }
 
+enum TransactionView {
+    Pending,
+    Received(Arc<Transaction>),
+}
+
 pub struct LinearLeiosNode {
+    id: NodeId,
+    sim_config: Arc<SimConfiguration>,
     queued: EventResult,
+    tracker: EventTracker,
+    consumers: Vec<NodeId>,
+    txs: HashMap<TransactionId, TransactionView>,
 }
 
 type EventResult = super::EventResult<Message, CpuTask>;
@@ -76,11 +85,16 @@ impl NodeImpl for LinearLeiosNode {
         config: &NodeConfiguration,
         sim_config: Arc<SimConfiguration>,
         tracker: EventTracker,
-        rng: ChaChaRng,
-        clock: Clock,
+        _rng: ChaChaRng,
+        _clock: Clock,
     ) -> Self {
         Self {
+            id: config.id,
+            sim_config,
             queued: EventResult::default(),
+            tracker,
+            consumers: config.consumers.clone(),
+            txs: HashMap::new(),
         }
     }
 
@@ -88,18 +102,75 @@ impl NodeImpl for LinearLeiosNode {
         std::mem::take(&mut self.queued)
     }
 
-    fn handle_new_tx(&mut self, _tx: Arc<Transaction>) -> EventResult {
-        // TODO: propagate TX
+    fn handle_new_tx(&mut self, tx: Arc<Transaction>) -> EventResult {
+        self.generate_tx(tx);
         std::mem::take(&mut self.queued)
     }
 
-    fn handle_message(&mut self, _from: NodeId, _msg: Self::Message) -> EventResult {
-        // TODO: propagate TX
+    fn handle_message(&mut self, from: NodeId, msg: Self::Message) -> EventResult {
+        match msg {
+            // TX propagation
+            Message::AnnounceTx(id) => self.receive_announce_tx(from, id),
+            Message::RequestTx(id) => self.receive_request_tx(from, id),
+            Message::Tx(tx) => self.receive_tx(from, tx),
+        }
         std::mem::take(&mut self.queued)
     }
 
-    fn handle_cpu_task(&mut self, _task: Self::Task) -> EventResult {
-        // TODO: propagate TX
+    fn handle_cpu_task(&mut self, task: Self::Task) -> EventResult {
+        match task {
+            CpuTask::TransactionValidated(from, tx) => self.propagate_tx(from, tx),
+        }
         std::mem::take(&mut self.queued)
+    }
+}
+
+impl LinearLeiosNode {
+    fn receive_announce_tx(&mut self, from: NodeId, id: TransactionId) {
+        if self.txs.get(&id).is_none_or(|t| {
+            self.sim_config.relay_strategy == RelayStrategy::RequestFromAll
+                && matches!(t, TransactionView::Pending)
+        }) {
+            self.txs.insert(id, TransactionView::Pending);
+            self.queued.send_to(from, Message::RequestTx(id));
+        }
+    }
+
+    fn receive_request_tx(&mut self, from: NodeId, id: TransactionId) {
+        if let Some(TransactionView::Received(tx)) = self.txs.get(&id) {
+            self.tracker.track_transaction_sent(tx, self.id, from);
+            self.queued.send_to(from, Message::Tx(tx.clone()));
+        }
+    }
+
+    fn receive_tx(&mut self, from: NodeId, tx: Arc<Transaction>) {
+        self.tracker
+            .track_transaction_received(tx.id, from, self.id);
+        self.queued
+            .schedule_cpu_task(CpuTask::TransactionValidated(from, tx));
+    }
+
+    fn generate_tx(&mut self, tx: Arc<Transaction>) {
+        self.tracker.track_transaction_generated(&tx, self.id);
+        self.propagate_tx(self.id, tx);
+    }
+
+    fn propagate_tx(&mut self, from: NodeId, tx: Arc<Transaction>) {
+        let id = tx.id;
+        if self
+            .txs
+            .insert(id, TransactionView::Received(tx.clone()))
+            .is_some_and(|tx| matches!(tx, TransactionView::Received(_)))
+        {
+            return;
+        }
+        // TODO: mempools
+        // TODO: should send to producers instead (make configurable)
+        for peer in &self.consumers {
+            if *peer == from {
+                continue;
+            }
+            self.queued.send_to(*peer, Message::AnnounceTx(id));
+        }
     }
 }
