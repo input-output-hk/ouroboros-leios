2bde3d39461dbc06fa44e84e4f241a74b51a5758
Brian W Bush <bbush@ip-172-31-50-249.us-west-2.compute.internal>
2025-07-19 16:18:36 +0000

    Completed simulations

--- analysis/sims/2025w27/combine-results.sh
+++ analysis/sims/2025w27/combine-results.sh
@@ -1,32 +1,41 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p gzip
+#!nix-shell -i bash -p gnused gzip pigz "rWrapper.override { packages = with rPackages; [ data_table R_utils bit64 ggplot2 magrittr stringr ]; }"
 
 set -e
 
-for d in tsr
+mkdir -p results
+for f in lifecycle resources receipts cpus
 do
-  mkdir -p results/$d
-  for f in cpus lifecycle resources receipts
-  do
-    DIR=$(find $d -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n -quit)
-    HL=$(sed -n -e '1p' "$DIR/case.csv")
-    HR=$(zcat "$DIR/$f.csv.gz" | sed -n -e '1p')
-    (
-      echo "$HL,$HR"
-      for g in $(find $d -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n)
-      do
-        if [ ! -e "$g/stderr" ]
-        then
-          echo "Skipping $g because it has no stderr." >> /dev/stderr
-        elif [ -s "$g/stderr" ]
-        then
-          echo "Skipping $g because its stderr is not empty." >> /dev/stderr
-        else
-          BL=$(sed -n -e '2p' "$g/case.csv")
-          zcat "$g/$f.csv.gz" | sed -e "1d;s/^/$BL,/;s/null/NA/g"
-        fi
-      done
-    ) | pigz -p 3 -9c > results/$d/$f.csv.gz &
-  done
+  DIR=$(find tsr -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n -quit)
+  HL=$(sed -n -e '1p' "$DIR/case.csv")
+  HR=$(zcat "$DIR/$f.csv.gz" | sed -n -e '1p')
+  if [[ "$f" == "lifecycle" || "$f" == "resources" ]]
+  then
+    FRACT=1.00
+  else
+    FRACT=0.20
+  fi
+  (
+    echo "$HL,$HR"
+    for g in $(find tsr -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n)
+    do
+      if [ ! -e "$g/stderr" ]
+      then
+        echo "Skipping $g because it has no stderr." >> /dev/stderr
+      elif [ -s "$g/stderr" ]
+      then
+        echo "Skipping $g because its stderr is not empty." >> /dev/stderr
+      else
+        BL=$(sed -n -e '2p' "$g/case.csv")
+        zcat "$g/$f.csv.gz" | gawk 'FNR > 1 && rand() <= '"$FRACT"' { print "'"$BL"'" "," $0}'
+      fi
+    done
+  ) | pigz -p 3 -9c > results/$f.csv.gz
+R --vanilla << EOI > /dev/null
+require(data.table)
+sampleSize <- $FRACT
+print(sampleSize)
+$f <- fread("results/$f.csv.gz", stringsAsFactors=TRUE)
+save($f, sampleSize, file="results/$f.Rdata", compression_level=9)
+EOI
 done
-wait
