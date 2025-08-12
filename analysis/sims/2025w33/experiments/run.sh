7aafcc4ee23b92b8aa12e1b03be54525f2359226
Brian W Bush <brian.bush@iohk.io>
2025-08-12 12:44:41 -0600

    Revised experimental design

--- analysis/sims/2025w33/experiments/run.sh
+++ analysis/sims/2025w33/experiments/run.sh
@@ -5,15 +5,15 @@ set -eo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-TX_START=0
-TX_STOP=1200
+TX_START=60
+TX_STOP=960
 SIM_STOP=1200
 BW=50
 CPU_COUNT=4
 NETWORK=topology-v2
 SIM=Rust
 VARIANT=linear-with-tx-references
-BLOCK_SIZE=10
+BLOCK_SIZE=12
 TX_SIZE=1500
 THROUGHPUT=0.150
 LABEL=$(basename "$PWD")
@@ -27,12 +27,11 @@ then
   TX_SPACING_HONEST=$(echo "scale=3; $TX_SIZE / $THROUGHPUT / 1000" | bc)
   TX_SPACING_ADVERSARY=0
 else
-  TX_SPACING_HONEST=$(echo "scale=3; $TX_SIZE / $THROUGHPUT / 665" | bc)
-  TX_SPACING_ADVERSARY=$(echo "scale=3; $TX_SIZE / $THROUGHPUT / 3" | bc)
+  TX_SPACING_HONEST=$(echo "scale=3; $TX_SIZE / $THROUGHPUT / 667" | bc)
+  TX_SPACING_ADVERSARY=$(echo "scale=3; $TX_SIZE / $THROUGHPUT / 3.70" | bc)
 fi
-####TX_SPACING_HONEST=9999999999
 
-ulimit -S -m 48000000 -v 48000000
+ulimit -S -m 80000000 -v 80000000
 
 if [[ -e sim.log ]]
 then
@@ -59,11 +58,25 @@ yaml2json ../config.yaml \
 , "tx-size-bytes-distribution": {distribution: "constant", value: '"$TX_SIZE"'}
 , "tx-generation-distribution": {distribution: "constant", value: '"$TX_SPACING_HONEST"'}
 , "late-eb-attack": {
-    "attackers":{"nodes":["node-48","node-49","node-50","node-531","node-486","node-529","node-525","node-526","node-487","node-475","node-476","node-98","node-519","node-520","node-485","node-12","node-18","node-497","node-498","node-527","node-528","node-414","node-415","node-60","node-61","node-91","node-92","node-93","node-94","node-95","node-96","node-97","node-57","node-58","node-59","node-215","node-599","node-604","node-99","node-100","node-582","node-583","node-584","node-585","node-530","node-532","node-416","node-499","node-500","node-566","node-567","node-568","node-101","node-569","node-570","node-571","node-572","node-573","node-13"]}
-  , "propagation-delay-ms": '"$EB_DELAY"'
+    "attackers": {
+      "nodes": (
+        if '"$EB_DELAY"' > 0
+        then ["node-48","node-49","node-50","node-531","node-486","node-529","node-525","node-526","node-487","node-475","node-476","node-98","node-519","node-520","node-485","node-12","node-18","node-497","node-498","node-527","node-528","node-414","node-415","node-60","node-61","node-91","node-92","node-93","node-94","node-95","node-96","node-97","node-57","node-58","node-59","node-215","node-599","node-604","node-99","node-100","node-582","node-583","node-584","node-585","node-530","node-532","node-416","node-499","node-500","node-566","node-567","node-568","node-101","node-569","node-570","node-571","node-572","node-573","node-13"]
+	  		else []
+	  		end
+      )
+    }
+  , "propagation-delay-ms": (1000 * '"$EB_DELAY"')
   }
 , "late-tx-attack": {
-    "attackers":{"nodes":["node-48","node-49","node-50","node-531","node-486","node-529","node-525","node-526","node-487","node-475","node-476","node-98","node-519","node-520","node-485","node-12","node-18","node-497","node-498","node-527","node-528","node-414","node-415","node-60","node-61","node-91","node-92","node-93","node-94","node-95","node-96","node-97","node-57","node-58","node-59","node-215","node-599","node-604","node-99","node-100","node-582","node-583","node-584","node-585","node-530","node-532","node-416","node-499","node-500","node-566","node-567","node-568","node-101","node-569","node-570","node-571","node-572","node-573","node-13"]}
+    "attackers": {
+      "nodes": (
+        if '"$TX_ATTACK"' > 0
+        then ["node-48","node-49","node-50","node-531","node-486","node-529","node-525","node-526","node-487","node-475","node-476","node-98","node-519","node-520","node-485","node-12","node-18","node-497","node-498","node-527","node-528","node-414","node-415","node-60","node-61","node-91","node-92","node-93","node-94","node-95","node-96","node-97","node-57","node-58","node-59","node-215","node-599","node-604","node-99","node-100","node-582","node-583","node-584","node-585","node-530","node-532","node-416","node-499","node-500","node-566","node-567","node-568","node-101","node-569","node-570","node-571","node-572","node-573","node-13"]
+	  		else []
+	  		end
+      )
+    }
   , "attack-probability": '"$TX_ATTACK"'
   , "tx-generation-distribution": {distribution: "constant", value: '"$TX_SPACING_ADVERSARY"'}
   }
@@ -94,8 +107,8 @@ esac
 wait
 
 cat << EOI > case.csv
-Simulator,Variant,Network,Bandwidth,CPU,Diffusion duration,Voting duration,Max EB size,Propagation,EB delay,Tx attack,Tx size,Throughput,Tx start [s],Tx stop [s],Sim stop [s]
-$SIM,$VARIANT,$NETWORK,$BW Mb/s,$CPU_COUNT vCPU/node,$STAGE_LENGTH_DIFF slot/stage,$STAGE_LENGTH_VOTE slot/stage,$BLOCK_SIZE MB/EB,$TX_SIZE B/Tx,$EB_DELAY s/EB,$TX_ATTACK %,$THROUGHPUT TxMB/s,$TX_START,$TX_STOP,$SIM_STOP
+Simulator,Propagation,Network,Bandwidth,CPU,Diffusion duration,Voting duration,Max EB size,Adversary,EB delay,Tx attack,Tx size,Throughput,Tx start [s],Tx stop [s],Sim stop [s]
+$SIM,$PROPAGATION,$NETWORK,$BW Mb/s,$CPU_COUNT vCPU/node,L_diff = $STAGE_LENGTH_DIFF slots,L_vote = $STAGE_LENGTH_VOTE slots,$BLOCK_SIZE MB/EB,33% adversary,EB attack: $(echo "$EB_DELAY s/EB" | sed -e 's@^0\.0 s/EB@none@'),Tx attack: $(echo "$((100 * TX_ATTACK))%" | sed -e 's@^0%@none@'),$TX_SIZE B/Tx,$THROUGHPUT TxMB/s,$TX_START,$TX_STOP,$SIM_STOP
 EOI
 
 zcat sim.log.gz \
