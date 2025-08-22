5e620a96d563f4e5b346402a1af0e20698d517a3
Brian W Bush <bbush@ip-172-31-50-249.us-west-2.compute.internal>
2025-08-22 13:43:42 +0000

    Analyzes experiment

--- analysis/sims/regression1/combine-results.sh
+++ analysis/sims/regression1/combine-results.sh
@@ -6,7 +6,7 @@ set -e
 mkdir -p results/$d
 for f in lifecycle resources receipts cpus sizes
 do
-  DIR=$(find experiments -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n -quit)
+  DIR=$(find experiment -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n -quit)
   HL=$(sed -n -e '1p' "$DIR/case.csv")
   HR=$(zcat "$DIR/$f.csv.gz" | sed -n -e '1p')
   if [[ "$f" == "lifecycle" || "$f" == "resources" || "$f" == "sizes" ]]
@@ -17,7 +17,7 @@ do
   fi
   (
     echo "$HL,$HR"
-    for g in $(find experiments -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n)
+    for g in $(find experiment -type f -name $f.csv.gz \( -not -empty \) -printf %h\\n)
     do
       if [ ! -e "$g/stderr" ]
       then
