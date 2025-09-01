0e6c91d38ce7edfefad7d22b8016b5cda83c20ae
will-break-it <will-break-it@users.noreply.github.com>
2025-09-01 10:09:03 +0000

    deploy: ecfde3876c83852fe97c7d24bdca117ad967cb18

---
+++ assets/js/02136c89.bfd9f16a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[4133],{3176:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/news/page/4","page":4,"postsPerPage":10,"totalPages":5,"totalCount":50,"previousPage":"/news/page/3","nextPage":"/news/page/5","blogDescription":"Blog","blogTitle":"News"}}')}}]);
