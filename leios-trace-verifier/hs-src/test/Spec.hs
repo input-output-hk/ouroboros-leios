4a7bcf8d5a1a3d56f29ae11306e09fe9eeee32dc
Brian W Bush <brian.bush@iohk.io>
2025-10-22 12:04:18 -0600

    Added code comments to leios trace verifier

--- leios-trace-verifier/hs-src/test/Spec.hs
+++ leios-trace-verifier/hs-src/test/Spec.hs
@@ -1,9 +1,12 @@
+--| Main entry point.
+
 module Main where
 
 import Spec.Generated (generated)
 import Spec.Golden (golden)
 import Test.Hspec (describe, hspec)
 
+-- | Test the trace verifier.
 main :: IO ()
 main =
   hspec $ do
