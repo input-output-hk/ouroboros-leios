61c2d45d2f8190d68c3a8edcefc5a91c3ab8b7ed
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-10-25 12:04:14 +0200

    Add nix derivation and github workflow for leios-design.pdf

--- nix/artifacts.nix
+++ nix/artifacts.nix
@@ -33,7 +33,40 @@ let
     '';
   };
 
+  leiosDesignPdf = pkgs.stdenv.mkDerivation {
+    name = "leios-design-pdf";
+    src = ../docs/leios-design;
+    buildInputs = [
+      pkgs.pandoc
+      pkgs.texliveFull
+      pkgs.librsvg
+      pkgs.typst
+    ];
+    buildPhase = ''
+      # Work directly in the source directory where all assets are available
+      cd $src
+      mkdir -p $out
+
+      # Convert markdown to PDF using pandoc with XeLaTeX
+      pandoc README.md \
+        --pdf-engine=xelatex \
+        --from=markdown \
+        --to=pdf \
+        --output=$out/leios-design.pdf \
+        --table-of-contents \
+        --number-sections \
+        --highlight-style=tango \
+        --variable fontsize=11pt \
+        --variable documentclass=article \
+        --variable geometry:margin=2.5cm \
+        --variable colorlinks=true \
+        --variable linkcolor=blue \
+        --variable urlcolor=blue \
+        --variable toccolor=black
+    '';
+  };
+
 in
 {
-  inherit simRealism networkSpec;
+  inherit simRealism networkSpec leiosDesignPdf;
 }
