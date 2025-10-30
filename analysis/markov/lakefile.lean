821e5284cfdcb84fc00234a1c35a10d3b8415bfd
Brian W Bush <brian.bush@iohk.io>
2025-10-30 12:03:33 -0600

    Switched to using lakefile.lean instead of lakefile.toml

---
+++ analysis/markov/lakefile.lean
@@ -0,0 +1,22 @@
+
+import Lake
+
+open Lake DSL
+
+package «linleios» where
+
+lean_lib «Linleios» where
+
+@[default_target]
+lean_exe «linleios» where
+  root := `Main
+  supportInterpreter := false
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4" @ "v4.20.0"
+
+require Parser from git
+  "https://github.com/fgdorais/lean4-parser" @ "26d5ce4d60195a869b1fdb100b442794ea63e1ad"
+
+require Cli from git
+  "https://github.com/mhuisi/lean4-cli" @ "v4.20.0"
