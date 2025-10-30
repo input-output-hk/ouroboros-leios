961bcf2dac2836f41c2e8c7e6922963caca37a7c
Brian W Bush <brian.bush@iohk.io>
2025-10-30 06:47:42 -0600

    Added code comments and documentation to markovian model (#591)
    * Added code comments to leios trace verifier
    * Added source documentation

--- analysis/markov/Linleios/Types.lean
+++ analysis/markov/Linleios/Types.lean
@@ -7,20 +7,44 @@ import Linleios.Probability
 open Std (HashMap)
 
 
+/--
+Represent probabilities as floating-point numbers.
+-/
 abbrev Probability := Float
 
-
+/--
+The constant parameters used for computing transitions.
+-/
 structure Environment where
+  /-- The active slot coefficient. -/
   activeSlotCoefficient : Probability
+  /-- The $L_\text{hdr}$ protocol parameter. -/
   Lheader : Nat
+  /-- The $L_\text{vote}$ protocol parameter. -/
   Lvote : Nat
+  /-- The $L_\text{diff}$ protocol parameter. -/
   Ldiff : Nat
+  /-- The probability that an RB occurs long enough after the previous RB so that an EB can be certified. -/
   pSpacingOkay : Probability
+  /-- The per-step quorum probability. -/
   pQuorum : Probability
+  /-- The probability that the EB and transaction data arrives too late to compute the ledger. -/
   pLate : Probability
+  /-- The fraction of adversarial stake. -/
   fAdversary : Float
 deriving Repr
 
+/--
+Create an environment.
+
+- `activeSlotCoefficient`: the active slot coefficient
+- `committeeSize`: the mean committee size
+- `τ`: the quorum fraction
+- `pRbHeaderArrives`: the probability that an RB header arrives before $L_\text{hdr}$ slots
+- `pEBValidates`: the probability that an EB is validated before $3 L_\text{hdr} + L_\text{vote} + L_\text{diff}$ slots
+- `pEbUnvalidated`: the probability that the EB and transaction data arrives too late to compute the ledger
+- `fAdversary`: the fraction of adversarial stake
+-/
 def makeEnvironment (Lheader Lvote Ldiff : Nat) (activeSlotCoefficient committeeSize τ pRbHeaderArrives pEbValidates pEbUnvalidated fAdversary : Float) : Environment :=
   {
     activeSlotCoefficient := activeSlotCoefficient
@@ -34,14 +58,25 @@ def makeEnvironment (Lheader Lvote Ldiff : Nat) (activeSlotCoefficient committee
   }
 
 
+/--
+The state of the chain's evolution.
+-/
 structure State where
+  /-- The number of potential RB oppurtunities. -/
   clock : Nat
+  /-- The number of RBs actually forged. -/
   rbCount : Nat
+  /-- The number of EBs certified. -/
   ebCount : Nat
+  /-- Whether an RB was forged in the current step. -/
   hasRb : Bool
+  /-- Whether an EB could be certified in the current step. -/
   canCertify : Bool
 deriving Repr, BEq, Hashable, Inhabited
 
+/--
+The genesis state starts when zero counts.
+-/
 theorem genesis : (default : State).clock = 0 ∧ (default : State).rbCount = 0 ∧ (default : State).ebCount = 0 := by
   constructor
   rfl
@@ -50,6 +85,9 @@ theorem genesis : (default : State).clock = 0 ∧ (default : State).rbCount = 0 
   rfl
 
 
+/--
+The active states and their probabilities.
+-/
 def Probabilities := HashMap State Probability
 deriving Repr, EmptyCollection
 
@@ -57,4 +95,6 @@ instance : Inhabited Probabilities where
   default := (∅ : Probabilities).insert Inhabited.default 1
 
 
+/--
+An outcome is a list of new states and their probabilities. -/
 abbrev Outcomes := List (State × Probability)
