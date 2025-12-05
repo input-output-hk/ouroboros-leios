27aada54f5c9ca220e9dc46eca43e67cb60e4536
Drazen Popovic <drazen.popovic@iohk.io>
2025-12-05 13:22:19 +0100

    Migrate leios-demo/extras

---
+++ demo/extras/http_command/build.nix
@@ -0,0 +1,48 @@
+{
+
+  perSystem =
+    {
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      packages = {
+        http_command_on_request = pkgs.writeShellApplication {
+          name = "on_request";
+          text = builtins.readFile ./on_request.sh;
+        };
+
+        http_command = pkgs.writeShellApplication {
+          name = "http_command";
+          runtimeInputs = [
+            pkgs.socat
+            config.packages.http_command_on_request
+          ];
+          text = builtins.readFile ./server.sh;
+        };
+      };
+
+      checks.test_http_command = pkgs.stdenv.mkDerivation {
+        name = "test_http_command";
+        src = ./.;
+        buildInputs = [
+          pkgs.curl
+          config.packages.http_command
+        ];
+        buildPhase = ''
+          touch $out
+          before=$(ls)
+          http_command 127.0.0.1 1337 "ls" &
+          sleep 1
+          after=$(curl 127.0.0.1:1337)
+          if [[ "$before" != "$after" ]]; then
+              echo "Before and after should match"
+              echo before: $before
+              echo after: $after
+              exit 1
+          fi
+        '';
+      };
+    };
+}
---
+++ demo/extras/ss_to_prometheus/build.nix
@@ -0,0 +1,91 @@
+{
+
+  perSystem =
+    {
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      packages = {
+
+        ss_prom = pkgs.writeShellApplication {
+          name = "ss_prom";
+
+          runtimeInputs = [
+            pkgs.procps
+            pkgs.coreutils
+            pkgs.iproute2
+          ];
+
+          text = builtins.readFile ./ss_prom.sh;
+        };
+
+        ss_http_exporter = pkgs.writeShellApplication {
+          name = "ss_http_exporter";
+
+          runtimeInputs = [
+            config.packages.http_command
+            config.packages.ss_prom
+          ];
+
+          text = builtins.readFile ./ss_http_exporter.sh;
+        };
+      };
+
+      checks = {
+
+        test_ss_to_prometheus = pkgs.stdenv.mkDerivation {
+          name = "test_ss_to_prometheus";
+          src = ./.;
+          buildPhase = ''
+            . ${./ss_prom.sh}
+            cat ${./test/dport_https_sockets.ss} | ss_to_prometheus > result.prom
+            diff result.prom ${./test/dport_https_sockets.prom}
+            touch $out
+          '';
+        };
+
+        test_ss_prom = pkgs.stdenv.mkDerivation {
+          name = "test_ss_prom";
+          src = ./.;
+          buildInputs = [
+            config.packages.ss_prom
+            pkgs.netcat
+          ];
+          buildPhase = ''
+            nc -l 1234 &
+            nc -p 1337 127.0.0.1 1234 &
+            sleep 2
+            ss_prom "dport = 1234" > result.prom
+            cat result.prom | grep -v "#" | cut -d " " -f 1 > got
+            cat ${./test/localhost_1234.prom} | grep -v "#" | cut -d " " -f 1 > wanted
+            diff wanted got
+            mv result.prom $out
+          '';
+        };
+
+        test_ss_http_exporter = pkgs.stdenv.mkDerivation {
+          name = "test_ss_http_exporter";
+          src = ./.;
+          buildInputs = [
+            config.packages.ss_http_exporter
+            pkgs.curl
+            pkgs.netcat
+          ];
+          buildPhase = ''
+            nc -l 1234 &
+            nc -p 1337 127.0.0.1 1234 &
+            ss_http_exporter 127.0.0.1 9100 "dport = 1234" &
+            sleep 2
+            curl 127.0.0.1:9100 > result.prom
+            cat result.prom | grep -v "#" | cut -d " " -f 1 > got
+            cat ${./test/localhost_1234.prom} | grep -v "#" | cut -d " " -f 1 > wanted
+            diff wanted got
+            mv result.prom $out
+          '';
+        };
+
+      };
+    };
+}
