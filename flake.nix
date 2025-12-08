170ef6f4652ace4441e745f4811e1953e5c70a51
Drazen Popovic <drazen.popovic@iohk.io>
2025-12-08 17:48:48 +0100

    Enable only x86_64-linux Hydra job

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
 
   outputs =
     inputs@{
+      self,
       nixpkgs,
       flake-parts,
       ...
@@ -55,18 +56,17 @@
         "aarch64-darwin"
       ];
 
-      flake.hydraJobs = {};
       # TODO(bladyjoker): Enable when Hydra is back to stable
-      # flake.hydraJobs = import ./nix/hydra.nix {
-      #   flake = self;
-      #   inherit lib;
-      #   systems = [
-      #     "x86_64-linux"
-      #     "x86_64-darwin"
-      #     "aarch64-linux"
-      #     "aarch64-darwin"
-      #   ];
-      # };
+      flake.hydraJobs = import ./nix/hydra.nix {
+        flake = self;
+        inherit lib;
+        systems = [
+          "x86_64-linux"
+          # "x86_64-darwin"
+          # "aarch64-linux"
+          # "aarch64-darwin"
+        ];
+      };
 
     };
 
