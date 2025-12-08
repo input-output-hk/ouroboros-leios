3c54f693e8a9044109788d9eb9981f198a259b2c
Drazen Popovic <drazen.popovic@iohk.io>
2025-12-08 16:23:16 +0100

    Draft the nix flake check GH workflow

--- flake.nix
+++ flake.nix
@@ -56,16 +56,16 @@
         "aarch64-darwin"
       ];
 
-      flake.hydraJobs = import ./nix/hydra.nix {
-        flake = self;
-        inherit lib;
-        systems = [
-          "x86_64-linux"
-          "x86_64-darwin"
-          "aarch64-linux"
-          "aarch64-darwin"
-        ];
-      };
+      # flake.hydraJobs = import ./nix/hydra.nix {
+      #   flake = self;
+      #   inherit lib;
+      #   systems = [
+      #     "x86_64-linux"
+      #     "x86_64-darwin"
+      #     "aarch64-linux"
+      #     "aarch64-darwin"
+      #   ];
+      # };
 
     };
 
