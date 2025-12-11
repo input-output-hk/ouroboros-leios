0d1a647c689fc06ab852b8597a96d05bbb0ed413
Drazen Popovic <drazen.popovic@iohk.io>
2025-12-11 17:08:17 +0100

    Enable all systems

--- flake.nix
+++ flake.nix
@@ -55,9 +55,9 @@
 
       systems = [
         "x86_64-linux"
-        # "x86_64-darwin"
-        # "aarch64-linux"
-        # "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
 
       flake.hydraJobs = import ./nix/hydra.nix {
@@ -65,9 +65,9 @@
         inherit lib;
         systems = [
           "x86_64-linux"
-          # "x86_64-darwin"
-          # "aarch64-linux"
-          # "aarch64-darwin"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
         ];
       };
 
