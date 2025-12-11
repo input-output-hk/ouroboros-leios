d8e1c84f4e42c90f8c5421dfbb56ef5591d365a4
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-12-11 22:34:37 +0100

    Add a .#ui-live derivation to open ui with live stream

--- ui/build.nix
+++ ui/build.nix
@@ -1,8 +1,11 @@
 {
   perSystem =
     { pkgs, ... }:
-    # Resolve symlinks
     let
+      # NOTE: This requires the whole repository as context to resolve symlinks
+      # properly. There is no easy way to restrict the derivation inputs
+      # meaningfully so we need to accept invalidation of this on any change on
+      # the repo (or enumerate all the symlinks here).
       resolvedSrc = pkgs.stdenv.mkDerivation {
         name = "leios-ui-source-resolved";
         src = ../.;
@@ -12,7 +15,7 @@
         '';
       };
     in
-    {
+    rec {
       packages.ui = pkgs.buildNpmPackage {
         name = "leios-ui";
         src = resolvedSrc;
@@ -24,8 +27,16 @@
           npm run build
         '';
         installPhase = ''
-          mkdir -p $out
-          cp -r dist/* $out/
+          mkdir -p $out/visualizer/
+          cp -r dist/* $out/visualizer/
+        '';
+      };
+
+      packages.ui-live = pkgs.writeShellApplication {
+        name = "leios-ui-live";
+        runtimeInputs = [ pkgs.http-server ];
+        text = ''
+          http-server ${packages.ui} -o /visualizer/?scenario=2
         '';
       };
     };
