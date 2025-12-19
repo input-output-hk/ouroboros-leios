815782472129c8f88ff5c8f9b179274a9196cc6e
Yves Hauser <yves.hauser@iohk.io>
2025-12-19 13:10:24 +0100

    Applied formatting, as `shfmt` failed in CI

--- demo/2025-11/scripts/prepare-upstream-node.sh
+++ demo/2025-11/scripts/prepare-upstream-node.sh
@@ -2,8 +2,8 @@ set -exuo pipefail
 set -a && source "$WORKING_DIR/.env" && set +a
 
 if [ -d "$UPSTREAM_NODE_DIR" ]; then
-	echo "Removing old $UPSTREAM_NODE_DIR"
-	rm -fr "$UPSTREAM_NODE_DIR"
+  echo "Removing old $UPSTREAM_NODE_DIR"
+  rm -fr "$UPSTREAM_NODE_DIR"
 fi
 
 mkdir "$UPSTREAM_NODE_DIR"
@@ -12,16 +12,16 @@ echo "Working directory created for upstream-node: $UPSTREAM_NODE_DIR"
 echo "Generate the Leios DB and the schedules"
 
 leiosdemo202510 generate \
-	"$UPSTREAM_NODE_DIR/leios.db" \
-	"$LEIOS_MANIFEST" \
-	"$UPSTREAM_NODE_DIR/base-schedule.json"
+  "$UPSTREAM_NODE_DIR/leios.db" \
+  "$LEIOS_MANIFEST" \
+  "$UPSTREAM_NODE_DIR/base-schedule.json"
 
 if [[ ! -v LEIOS_SCHEDULE ]]; then
-	# FIXME: Make this release time configurable
-	jq "map(.[0] = 128.9)" "$UPSTREAM_NODE_DIR/base-schedule.json" >"$UPSTREAM_NODE_DIR/schedule.json"
-	echo "LEIOS_SCHEDULE not set, using default"
+  # FIXME: Make this release time configurable
+  jq "map(.[0] = 128.9)" "$UPSTREAM_NODE_DIR/base-schedule.json" >"$UPSTREAM_NODE_DIR/schedule.json"
+  echo "LEIOS_SCHEDULE not set, using default"
 else
-	cp -f "$LEIOS_SCHEDULE" "$UPSTREAM_NODE_DIR/schedule.json"
+  cp -f "$LEIOS_SCHEDULE" "$UPSTREAM_NODE_DIR/schedule.json"
 fi
 
 cp -f "$DATA_DIR/upstream-node/config.json" "$UPSTREAM_NODE_DIR/config.json"
