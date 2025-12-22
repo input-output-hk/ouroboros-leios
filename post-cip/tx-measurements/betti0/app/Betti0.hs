62fe41c928bda234c637a710b57c7cedf86d8353
Brian W Bush <brian.bush@iohk.io>
2025-12-22 07:59:20 -0700

    Streamlined data pipeline

--- post-cip/tx-measurements/betti0/app/Betti0.hs
+++ post-cip/tx-measurements/betti0/app/Betti0.hs
@@ -8,7 +8,7 @@ module Main where
 import Control.Monad (forM_, when, zipWithM_)
 import Control.Monad.ST (ST, runST)
 import Data.Array.ST (STUArray, newArray, readArray, writeArray)
-import Data.List (intercalate)
+import Data.List (intercalate, partition)
 import System.Environment (getArgs)
 import System.IO (IOMode(WriteMode), hClose, hPutStrLn, openFile, stderr)
 
@@ -44,11 +44,16 @@ readTsv getWeight maxWeight path = do
       parse :: [String] -> ((TxId, TxId), Weight)
       parse [_, tc, ts, ls, bs] = ((read tc, read ts), getWeight (read ls, read bs))
       parse x = error $ show x
-      edges =
-          takeWhile ((<= maxWeight) . snd)
+      (edges, rest) =
+          partition ((<= maxWeight) . snd)
         . fmap (parse . fmap BL8.unpack . BL8.split '\t')
         $ drop 1 rows
-      vertices = V.fromList . S.toList $ S.fromList (fst . fst <$> edges) <> S.fromList (snd . fst <$> edges)
+      vertexSet =
+        foldl
+          (\acc row -> S.insert (fst $ fst row) . S.insert (snd $ fst row) $ acc)
+          (S.fromList (fst . fst <$> edges) <> S.fromList (snd . fst <$> edges))
+          $ take 0 rest
+      vertices = V.fromList $ S.toList vertexSet
       vertexMap = M.fromList $ zip (V.toList vertices) [0..]
       reindex :: ((TxId, TxId), Weight) -> Edge 
       reindex ((tc, ts), w) = Edge (vertexMap M.! tc) (vertexMap M.! ts) w
@@ -61,7 +66,6 @@ find parent i = do
   if p == i
     then pure i
     else do
-      -- Path Compression: Point i directly to the root
       root <- find parent p
       writeArray parent i root
       pure root
@@ -72,7 +76,6 @@ union parent rank i j = do
   rootJ <- find parent j
   if rootI /= rootJ
     then do
-      -- Union by Rank: Attach smaller tree to larger tree
       rankI <- readArray rank rootI
       rankJ <- readArray rank rootJ
       if rankI < rankJ
@@ -81,40 +84,28 @@ union parent rank i j = do
           writeArray parent rootJ rootI
           when (rankI == rankJ)
             $ writeArray rank rootI (rankI + 1)
-      -- Merged successfully
       pure True 
     else
-      -- Already in same component
       pure False
 
 computeBetti0 :: Int -> [Edge] -> [(Weight, Int)]
 computeBetti0 numNodes sortedEdges = runST $ do
-  -- Initialize Union-Find
   parent <- newArray (0, numNodes - 1) 0 :: ST s (STUArray s Int Int)
   rank   <- newArray (0, numNodes - 1) 0 :: ST s (STUArray s Int Int)
-  -- Initialize parent array so each node points to itself
   forM_ [0 .. numNodes - 1]
     $ \i -> writeArray parent i i
-  -- Iterate through edges and track Betti-0
-  -- Using a recursive helper to accumulate results
   let go [] _ = pure mempty
       go (e:es) currentCount = do
         merged <- union parent rank (u e) (v e)
         let newCount = if merged then currentCount - 1 else currentCount
-        -- Look ahead to handle edges with identical weights
-        -- We only emit a data point when the weight actually changes
         case es of
           (nextE : _) | w nextE == w e -> go es newCount
           _  -> ((w e, newCount) :) <$> go es newCount
-  -- Start with b0 = numNodes (everyone is isolated)
   go sortedEdges numNodes
 
 
--- Helper to get the canonical root for EVERY node
 getSnapshot :: STUArray s Int Int -> Int -> ST s [Int]
 getSnapshot parent numNodes = do
-  -- We map 'find' over every node index [0 .. n-1]
-  -- Note: This modifies the array (path compression), which is good!
   mapM (find parent) [0 .. numNodes - 1]
 
 computeMembership :: Int -> [Edge] -> [(Weight, [Int])]
@@ -138,8 +129,9 @@ computeMembership numNodes sortedEdges = runST $ do
 
 main :: IO ()
 main = do
- [wType, wMax, infile, outfile] <- getArgs
- let maxWeight = read wMax
+ wType : wMax : infile : rest <- getArgs
+ let doOutput = length rest > 0
+     maxWeight = read wMax
      getWeight :: (Weight, Weight) -> Weight
      getWeight =
        case wType of
@@ -151,12 +143,9 @@ main = do
  hPutStrLn stderr $ "Filter: " <> wType <> " lifetime <= " <> show maxWeight
  hPutStrLn stderr $ "Vertices: " <> show n
  hPutStrLn stderr $ "Edges: " <> show (length edges)
- if False
+ if doOutput
    then do
-     let bettiCurve = computeBetti0 n edges
-     putStrLn "Lifetime\tBetti-0"
-     mapM_ (\(w, b) -> putStrLn $ intercalate tab [show w, show b]) bettiCurve
-   else do
+     [outfile] <- pure rest
      let memberships = computeMembership n edges
      putStrLn "Lifetime\tBetti-0"
      h <- openFile outfile WriteMode
@@ -167,4 +156,8 @@ main = do
            [0..] components
          putStrLn $ intercalate tab [show w, show . S.size $ S.fromList components]
      hClose h
+   else do
+     let bettiCurve = computeBetti0 n edges
+     putStrLn "Lifetime\tBetti-0"
+     mapM_ (\(w, b) -> putStrLn $ intercalate tab [show w, show b]) bettiCurve
 
