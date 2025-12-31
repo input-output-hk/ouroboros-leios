b267affed7a2a2e475d990e2174209c5dce93067
Yves <yves.hauser@iohk.io>
2025-12-31 09:46:21 +0100

    Fixed formatting

--- analysis/sims/kernels.nix
+++ analysis/sims/kernels.nix
@@ -9,33 +9,34 @@
   };
   kernel.r.minimal = {
     enable = true;
-    extraRPackages = p: with p; [
-      bit64
-      cowplot
-      curl
-      data_table
-      dplyr
-      geosphere
-      ggExtra
-      ggpattern
-      ggplot2
-      ggraph
-    # ggtda
-      igraph
-      ltsa
-      lubridate
-      mapproj
-      maps
-      mongolite
-      poibin
-      quantreg
-      R_utils
-      ripserr
-      RPostgres
-      stringr
-      svglite
-      TDAstats
-      VGAM
-    ];
+    extraRPackages =
+      p: with p; [
+        bit64
+        cowplot
+        curl
+        data_table
+        dplyr
+        geosphere
+        ggExtra
+        ggpattern
+        ggplot2
+        ggraph
+        # ggtda
+        igraph
+        ltsa
+        lubridate
+        mapproj
+        maps
+        mongolite
+        poibin
+        quantreg
+        R_utils
+        ripserr
+        RPostgres
+        stringr
+        svglite
+        TDAstats
+        VGAM
+      ];
   };
 }
