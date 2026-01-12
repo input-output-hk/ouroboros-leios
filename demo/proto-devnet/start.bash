3683faae1407f74c7caff4bebce50215920bea6d
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-01-12 17:11:19 +0100

    Package steps to start devnet into a nix derivation

---
+++ demo/proto-devnet/start.bash
@@ -0,0 +1,21 @@
+set -eo pipefail
+
+if [ -d devnet ]; then
+	read -r -p "Environment devnet/ already exists. Delete it? (y/n): [y]" yn
+	case ${yn:-y} in
+	[Yy]*) rm -rf devnet ;;
+	[Nn]*)
+		echo "Aborted"
+		exit 1
+		;;
+	esac
+fi
+
+# We need to create an environment before starting it, because the prototype
+# `cardano-node` is a bit older than `cardano-testnet` and needs patching:
+
+cardano-testnet create-env --output devnet --num-pool-nodes 3 --slot-length 1 --testnet-magic 164 --params-mainnet
+
+cat devnet/configuration.yaml | jq '.EnableP2P = true' >devnet/configuration-p2p.yaml && mv devnet/configuration{-p2p,}.yaml
+
+cardano-testnet cardano --node-env devnet --update-time
