72727d39334f6eeeb3080601ceb4c21c6bb372c8
Brian W Bush <brian.bush@iohk.io>
2026-01-14 14:20:58 -0700

    Implemented addition of adversarial nodes

--- post-cip/mempool-sim-web/src/cli.ts
+++ post-cip/mempool-sim-web/src/cli.ts
@@ -1,19 +1,26 @@
-import { generateNetwork } from './generator.js';
+import { generateNetwork, addAdversaryNode } from './generator.js';
 
 const NODES = 50;
 const DEGREE = 6;
-const BLOCK_B = 90000;
-const MEMPOOL_B = 2 * BLOCK_B;
+const BLOCK = 90000;
+const MEMPOOL = 2 * BLOCK;
 const LATENCY = 0.100; // = 100 ms
-const BANDWIDTH = 125000; // = 10 Mb/s
+const BANDWIDTH = 1250000; // = 10 Mb/s
+
+const ADVERSARY_COUNT = 2;
+const ADVERSARY_DEGREE = 3 * DEGREE;
 
 try {
   console.log(`Generating a ${DEGREE}-regular graph with ${NODES} nodes...`);
-  const graph = generateNetwork(NODES, DEGREE, MEMPOOL_B, LATENCY, BANDWIDTH);
+  const graph = generateNetwork(NODES, DEGREE, MEMPOOL, LATENCY, BANDWIDTH);
 
   console.log("✅ Success!");
   console.log(`Nodes: ${graph.order}, Edges: ${graph.size}`);
   
+  for (let i = 0; i < ADVERSARY_COUNT; ++i) {
+    addAdversaryNode(graph, "A" + (i + 1), 3 * DEGREE, 3 * DEGREE, MEMPOOL, LATENCY, BANDWIDTH);
+  }
+
   console.log("\nAdjacency List:");
   graph.forEachNode((node) => {
     const neighbors = graph.outboundNeighbors(node);
