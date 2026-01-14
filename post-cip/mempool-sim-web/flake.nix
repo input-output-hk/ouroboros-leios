29962e83e634eb7e77601f8a6bf6b9652baf4b47
Brian W Bush <brian.bush@iohk.io>
2026-01-14 12:30:37 -0700

    Nix flake for typescript development environment

---
+++ post-cip/mempool-sim-web/flake.nix
@@ -0,0 +1,30 @@
+{
+
+  description = "TypeScript development environment for memory pool simulation.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            typescript
+          ];
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+            export PS1="\[\e[1;32m\][nix-shell:\w]$\[\e[m\] "
+            echo "Entering Nix shell with TypeScript tools . . . "
+          '';
+        };
+      }
+    );
+
+}
