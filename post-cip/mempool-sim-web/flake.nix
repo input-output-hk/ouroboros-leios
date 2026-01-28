a9a357690fbdbd907253386c669274b6d7d9bfa6
Drazen Popovic <drazen.popovic@iohk.io>
2026-01-28 09:39:37 +0100

    chore: Apply `nix fmt`

--- post-cip/mempool-sim-web/flake.nix
+++ post-cip/mempool-sim-web/flake.nix
@@ -7,8 +7,13 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
