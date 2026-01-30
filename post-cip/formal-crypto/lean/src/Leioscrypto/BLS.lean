7b728bdc672a3c06877acfc0d36e01149e0cd2c9
Brian W Bush <brian.bush@iohk.io>
2026-01-30 13:04:49 -0700

    Specified core BLS functionality

--- post-cip/formal-crypto/lean/src/Leioscrypto/BLS.lean
+++ post-cip/formal-crypto/lean/src/Leioscrypto/BLS.lean
@@ -2,43 +2,97 @@
 namespace Leioscrypto.BLS
 
 
-namespace G1
-
-  def Point := Fin (2^192)
-  deriving BEq
-
-  opaque Point.WellFormed : Point → Prop
-
-end G1
+def dstLeios : ByteArray := "Leios".toUTF8
 
 
-namespace G2
-
-  def Point := Fin (2^96)
-  deriving BEq
-
-  opaque Point.WellFormed : Point → Prop
-
-end G2
+def SecretKey := Vector UInt8 256
+deriving Inhabited
 
 
-def PublicKey := G2.Point
-  deriving BEq
+namespace Spec
+
+  def G1.Point := Vector UInt8 48
+  deriving Inhabited
+
+  -- Group multiplication.
+  opaque G1.product : List G1.Point → G1.Point
+
+  def G2.Point := Vector UInt8 96
+  deriving Inhabited
+
+  -- Group multiplication.
+  opaque G2.product : List G2.Point → G2.Point
+
+  abbrev PublicKey := G2.Point
+
+  abbrev Signature := G1.Point
+
+  -- https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-06#name-keygen
+  opaque KeyGen (seed : ByteArray) : seed.size ≥ 32 → SecretKey
+
+  -- https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-06#name-sktopk
+  opaque SkToPk : SecretKey → PublicKey
+
+  -- https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-06#name-keyvalidate
+  opaque KeyValidate : PublicKey → Prop
+
+  -- https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-06#name-coresign
+  opaque CoreSign : SecretKey → ByteArray → Signature
+
+  -- https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-06#name-coreverify
+  opaque CoreVerify : PublicKey → ByteArray → Signature → Prop
+
+end Spec
 
 
-def PoP := G2.Point
-  deriving BEq
+def PublicKey := Spec.G2.Point
+
+def PublicKey.WellFormed : PublicKey → Prop :=
+  Spec.KeyValidate
+
+private def PublicKey.toByteArray : PublicKey → ByteArray :=
+  ByteArray.mk ∘ Vector.toArray
+
+structure ProofOfPossession where
+  μ₁ : Spec.G1.Point
+  μ₂ : Spec.G1.Point
+
+private def PublicKey.popMessage (mvk : PublicKey) : ByteArray :=
+  dstLeios ++ "PoP".toUTF8 ++ mvk.toByteArray
+
+def KeyGen (sk : SecretKey) : PublicKey × ProofOfPossession :=
+  let mvk : PublicKey := Spec.SkToPk sk
+  let μ₁ := Spec.CoreSign sk mvk.popMessage
+  let μ₂ := Spec.CoreSign sk dstLeios
+  ⟨ mvk , ⟨ μ₁ , μ₂ ⟩ ⟩
+
+def Check : PublicKey → ProofOfPossession → Prop
+| mvk , ⟨ μ₁ , μ₂ ⟩ =>
+    let b₁ := Spec.CoreVerify mvk mvk.popMessage μ₁
+    let b₂ := Spec.CoreVerify mvk dstLeios μ₂
+    b₁ ∧ b₂
 
 
-def Signature := G1.Point
+def Signature := Spec.G1.Point
 
-namespace Signature
+private def Signature.toByteArray : Signature → ByteArray :=
+  ByteArray.mk ∘ Vector.toArray
 
-  def WellFormed : Signature → Prop := G1.Point.WellFormed
+private def sigMessage (m : ByteArray) : ByteArray :=
+  dstLeios ++ "M".toUTF8 ++ m
 
-end Signature
+def Sign (sk : SecretKey) (m : ByteArray) : Signature :=
+  Spec.CoreSign sk (sigMessage m)
 
-opaque verify : ByteArray → PublicKey → Signature → Prop
+def Ver (mvk : PublicKey) (m : ByteArray) (σ : Signature) : Prop :=
+  Spec.CoreVerify mvk (sigMessage m) σ
+
+
+def AKey : List PublicKey → PublicKey :=
+  Spec.G2.product
+
+def ASig : List Signature → Signature :=
+  Spec.G1.product
 
 
 end Leioscrypto.BLS
