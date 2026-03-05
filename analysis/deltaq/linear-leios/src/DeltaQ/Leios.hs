363f8e8b0057888058d0b85a85fb20e648baa6ac
Yves Hauser <yves.hauser@iohk.io>
2026-03-05 14:12:21 +0100

    Empirical distributions

--- analysis/deltaq/linear-leios/src/DeltaQ/Leios.hs
+++ analysis/deltaq/linear-leios/src/DeltaQ/Leios.hs
@@ -86,10 +86,10 @@ fetchingTx p =
     [ (hitRate, wait 0.001)
     , (1 - hitRate, blendedDelay B1024)
     ]
-  where
-    -- Steady-state hit rate
-    hitRate :: Rational
-    hitRate = toRational $ 2 * p / (2 + p)
+ where
+  -- Steady-state hit rate
+  hitRate :: Rational
+  hitRate = toRational $ 2 * p / (2 + p)
 
 -- | fetchingTxs
 --
@@ -100,13 +100,13 @@ fetchingTxs =
     [ (hitRate, wait 0.001)
     , (1 - hitRate, blendedDelay B1024)
     ]
-  where
-    π_1 = (2 -p) / (2 + p)
-    π_2 = 2 * p / (2 + p)
-    p = 1.0
+ where
+  π_1 = (2 - p) / (2 + p)
+  π_2 = 2 * p / (2 + p)
+  p = 1.0
 
-    hitRate :: Rational
-    hitRate = π_2 * p + π_1 * (1 - p)
+  hitRate :: Rational
+  hitRate = π_2 * p + π_1 * (1 - p)
 
 processRBandEB :: DQ
 processRBandEB = processRB ./\. processEB
--- analysis/deltaq/linear-leios/src/DeltaQ/Leios/EmpiricalDistributions.hs
+++ analysis/deltaq/linear-leios/src/DeltaQ/Leios/EmpiricalDistributions.hs
@@ -8,8 +8,8 @@ module DeltaQ.Leios.EmpiricalDistributions (
 ) where
 
 import Data.List (sort)
-import DeltaQ
-import DeltaQ.Distributions
+import DeltaQ (DQ, choices)
+import DeltaQ.Distributions (logNormalDQ, measuredDQ)
 
 -- | 'empiricalDQ'
 empiricalDQ :: [Rational] -> DQ
@@ -19,11 +19,53 @@ empiricalDQ ms =
       ([0, (1 / fromIntegral (length ms)) .. 1])
       (sort ms)
 
+-- | applyTx
+--
+-- Data from [block-edf.csv](https://github.com/input-output-hk/ouroboros-leios/blob/main/post-cip/empirical-distributions/block-edf.csv)
+--
+-- Distribution of apply:
+--
+-- +----------+--------+
+-- | \< 5 ms  | 28.20% |
+-- +----------+--------+
+-- | 5-10 ms  | 37.32% |
+-- +----------+--------+
+-- | 10-20 ms | 26.68% |
+-- +----------+--------+
+-- | \> 20 ms | 7.80%  |
+-- +----------+--------+
 applyTx :: DQ
-applyTx = logNormalDQ 0 1 -- FIXME: concurrentUpToN maxTxRefsInEB reapplyTx
+applyTx =
+  measuredDQ
+    [ (28.20, 0.005)
+    , (37.32, 0.01)
+    , (26.68, 0.02)
+    , (7.80, 0.03)
+    ]
 
+-- | reapplyTx
+--
+-- Data from [block-edf.csv](https://github.com/input-output-hk/ouroboros-leios/blob/main/post-cip/empirical-distributions/block-edf.csv)
+--
+-- Distribution of reapply:
+--
+-- +----------+--------+
+-- | \< 1 ms  | 41.85% |
+-- +----------+--------+
+-- | 1–3 ms   | 48.99% |
+-- +----------+--------+
+-- | 3–10 ms  | 7.98%  |
+-- +----------+--------+
+-- | \> 10 ms | 1.17%  |
+-- +----------+--------+
 reapplyTx :: DQ
-reapplyTx = logNormalDQ 0 1 -- FIXME: concurrentUpToN maxTxRefsInEB reapplyTx
+reapplyTx =
+  measuredDQ
+    [ (41.85, 0.001)
+    , (48.99, 0.003)
+    , (7.98, 0.01)
+    , (1.17, 0.02)
+    ]
 
 applyTxs :: DQ
 applyTxs = logNormalDQ 0 1 -- FIXME: concurrentUpToN maxTxRefsInEB reapplyTx
