96929c12c0c814dcb5d38597aebf6e6ef3f03141
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2026-03-05 11:56:28 +0100

    demo: fix network emulation

--- demo/2025-11/scripts/ss-on-downstream.sh
+++ demo/2025-11/scripts/ss-on-downstream.sh
@@ -1,3 +1,3 @@
 set -exuo pipefail
 
-ss_http_exporter "$IP_DOWNSTREAM_NODE" 9100 "( sport = $PORT_NODE0 and dport = $PORT_DOWNSTREAM_NODE ) or ( sport = $PORT_DOWNSTREAM_NODE and dport = $PORT_NODE0 )"
+ss_http_exporter "$IP_DOWNSTREAM_NODE" 9100 "( dst = $IP_NODE0 )"
