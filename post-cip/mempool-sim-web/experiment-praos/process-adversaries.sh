40f71686d91e687d2792b6c8271d9180f4d3adab
Brian W Bush <brian.bush@iohk.io>
2026-03-05 06:57:48 -0700

    Small Praos experiment

---
+++ post-cip/mempool-sim-web/experiment-praos/process-adversaries.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+(
+
+echo $'Adversarial nodes\tRB ID\tRB honest txs\tRB adversarial txs\tEB ID\tEB honest txs\tEB adversarial txs'
+for f in adversaries-*.jsonl.gz
+do
+  zcat $f \
+| jq -rs '
+    .[0].adversaries as $adversaries
+  | .[]
+  | select(.msg == "block produced")
+  | [
+      $adversaries
+    , .blockId
+    , .honestCount
+    , .adversarialCount
+    , .certifiedEB.ebId // ""
+    , .certifiedEB.honestCount // 0
+    , .certifiedEB.adversarialCount // 0
+    ]
+  | @tsv
+'
+done
+
+) | gzip -9c > adversaries.tsv.gz
---
+++ post-cip/mempool-sim-web/experiment-praos/run-adversaries.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+TX_DURATION=300
+TX_SIZE=1500
+TX_RATE=6
+TX_COUNT=$((TX_RATE * TX_DURATION))
+TX_LOAD=$((TX_RATE * TX_SIZE / 1000))
+SLOTS=$((TX_DURATION + 60))
+
+truncate -s 0 jobs-adversaries.list
+
+for ADVERSARIES in $(seq 0 5 500)
+do
+  echo "npm run cli -- --nodes 10000 --degree 20 --adversaries $ADVERSARIES --adversary-degree 20 --tx-size $TX_SIZE --tx-load $TX_LOAD --tx-duration $TX_DURATION --slots $SLOTS --log-target pino/file | tail -n +5 | gzip -9c > adversaries-$ADVERSARIES.jsonl.gz" >> jobs-adversaries.list
+done
+
+parallel --jobs=$(($(grep '^processor' /proc/cpuinfo | wc -l) * 1 / 2)) < jobs-adversaries.list
