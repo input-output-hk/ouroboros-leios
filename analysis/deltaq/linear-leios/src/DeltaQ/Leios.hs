6f4d258e387625e5074c6c020a4b3ef8a095fa93
Yves Hauser <yves.hauser@iohk.io>
2026-03-05 09:08:58 +0100

    haddock

--- analysis/deltaq/linear-leios/src/DeltaQ/Leios.hs
+++ analysis/deltaq/linear-leios/src/DeltaQ/Leios.hs
@@ -7,6 +7,9 @@
 module DeltaQ.Leios (
   -- * DeltaQ
   validateEB,
+  fetchingEB,
+  fetchingTx,
+  fetchingTxs,
 
   -- * Estimate protocol parameters
   lVoteEstimated,
@@ -53,40 +56,40 @@ maxTxsFetched = 1
 fetchingEB :: DQ
 fetchingEB = choices [(1, blendedDelay B512), (1, blendedDelay B1024), (1, blendedDelay B2048)] -- TODO: Model FFD
 
--- Markov model that was presented by Nick in the Leios monthly
+-- | fetchingTx
+--
+-- Markov model for the TxCache that was presented by Nick in the Leios monthly
 -- meeting in February:
 --
--- Transition matrix M:
+-- Transition matrix: \(M = \begin{pmatrix} 1-p & p \\ 1-p/2 & p/2 \end{pmatrix}\)
 --
--- [ 1−p    p   ]
--- [ 1-p/2  p/2 ]
+-- Stationary condition: \(π*M = π\)
 --
--- Stationary condition: π*M = π
+-- Solution: \(π_1 = (2-p)/(2+p), π_2 = 2p/(2+p)\)
 --
--- Solution:
---    π_1 = (2-p)/(2+p)
---    π_2 = 2p/(2+p)
---
--- +-----+-------+-------+
--- | p   | π_1   | π_2   |
--- |-----+-------+-------|
--- | 0.1 | 0.905 | 0.095 |
--- | 0.3 | 0.778 | 0.222 |
--- | 0.5 | 0.600 | 0.400 |
--- | 0.8 | 0.385 | 0.615 |
--- | 1.0 | 0.333 | 0.667 |
--- +-----+-------+-------+
-
--- Steady-state hit rate
-hitRate :: Double -> Rational
-hitRate p = toRational $ 2 * p / (2 + p)
-
+-- +-----+---------+---------+
+-- | p   | \(π_1\) | \(π_2\) |
+-- +=====+=========+=========+
+-- | 0.1 | 0.905   | 0.095   |
+-- +-----+---------+---------+
+-- | 0.3 | 0.778   | 0.222   |
+-- +-----+---------+---------+
+-- | 0.5 | 0.600   | 0.400   |
+-- +-----+---------+---------+
+-- | 0.8 | 0.385   | 0.615   |
+-- +-----+---------+---------+
+-- | 1.0 | 0.333   | 0.667   |
+-- +-----+---------+---------+
 fetchingTx :: Double -> DQ
 fetchingTx p =
   choices
-    [ (hitRate p, wait 0.001)
-    , (1 - hitRate p, blendedDelay B1024)
+    [ (hitRate, wait 0.001)
+    , (1 - hitRate, blendedDelay B1024)
     ]
+  where
+    -- Steady-state hit rate
+    hitRate :: Rational
+    hitRate = toRational $ 2 * p / (2 + p)
 
 fetchingTxs :: DQ
 fetchingTxs =
