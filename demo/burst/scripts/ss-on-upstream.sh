316af502a9e63afa743010636a968a14b3a01a6d
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2026-03-06 21:57:53 +0100

    demo: update socket statistics

--- demo/burst/scripts/ss-on-upstream.sh
+++ demo/burst/scripts/ss-on-upstream.sh
@@ -1,3 +1,4 @@
 set -exuo pipefail
 
-ss_http_exporter "$IP_UPSTREAM_NODE" 9100 "( dst = $IP_NODE0 )"
+ss_http_exporter "$IP_UPSTREAM_NODE" 9100 "( dst = $IP_NODE0 )" &
+{ while true; do date; ss -i -m -t sport = :3001; sleep 0.1s; done; } 2>&1 >${WORKING_DIR}/ss-upstream.log
