07585c14cd0ec7de3d04004029d2de3b4409e585
Marcin Wójtowicz <marcin.wojtowicz@iohk.io>
2026-03-06 21:57:53 +0100

    demo: update socket statistics

--- demo/2025-11/scripts/ss-on-node0.sh
+++ demo/2025-11/scripts/ss-on-node0.sh
@@ -1,3 +1,4 @@
 set -exuo pipefail
 
-ss_http_exporter "$IP_NODE0" 9100 "( dst = $IP_DOWNSTREAM_NODE ) or ( dst = $IP_UPSTREAM_NODE )"
+ss_http_exporter "$IP_NODE0" 9100 "( dst = $IP_DOWNSTREAM_NODE ) or ( dst = $IP_UPSTREAM_NODE )" &
+{ while true; do date; ss -i -m -t sport = :3001; sleep 0.1s; done; } 2>&1 >${WORKING_DIR}/ss-node0.log
