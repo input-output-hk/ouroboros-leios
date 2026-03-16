ff0d99a465607e7e4a4337ebec59a719432bc6ca
Filip Stoyanov <filip.stoyanov@iohk.io>
2026-03-16 14:56:23 +0000

    filip(feat): add iframe design implementation with latest main changes

--- site/src/components/icons/index.ts
+++ site/src/components/icons/index.ts
@@ -1,2 +1,7 @@
 export { default as ArrowRightIcon } from "./ArrowRightIcon";
+export { default as DiscordIcon } from "./DiscordIcon";
+export { default as FacebookIcon } from "./FacebookIcon";
 export { default as FileIcon } from "./FileIcon";
+export { default as GitHubIcon } from "./GithubIcon";
+export { default as LinkedInIcon } from "./LinkedInIcon";
+export { default as TwitterIcon } from "./TwitterIcon";
