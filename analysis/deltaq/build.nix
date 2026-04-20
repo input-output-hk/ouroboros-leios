4d6787c5e94564544f43890a7c2669a9d9a995e2
Sebastian Nagel <sebastian.nagel@ncoding.at>
2026-04-20 10:46:46 +0200

    Integrate deltaq-jupyter workflow to main-flake
    This makes is less surprising that a nix develop or direnv works
    differently from within analysis/deltaq/linear-leios

--- analysis/deltaq/build.nix
+++ analysis/deltaq/build.nix
@@ -1,43 +1,26 @@
 {
-  description = "DeltaQ for Jupyter and Docker";
+  inputs,
+  self,
+  lib,
+  ...
+}:
+{
+  perSystem =
+    { system, ... }:
 
-  nixConfig.extra-substituters = [
-    "https://tweag-jupyter.cachix.org"
-  ];
-  nixConfig.extra-trusted-public-keys = [
-    "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g="
-  ];
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-compat.url = "github:edolstra/flake-compat";
-    flake-compat.flake = false;
-    jupyenv.url = "github:tweag/jupyenv?ref=0c86802aaa3ffd3e48c6f0e7403031c9168a8be2";
-    deltaq.url = "github:DeltaQ-SD/deltaq";
-    deltaq.flake = false;
-  };
-
-  outputs =
-    {
-      self,
-      flake-utils,
-      nixpkgs,
-      jupyenv,
-      deltaq,
-      ...
-    }:
-    flake-utils.lib.eachSystem (with flake-utils.lib.system; [ x86_64-linux ]) (
-      system:
+    lib.optionalAttrs (system == "x86_64-linux") (
       let
+        inherit (inputs) nixpkgs;
 
         overlay = _next: prev: {
           haskell = prev.haskell // {
             packageOverrides = _hnext: hprev: {
               # Include the DeltaQ packages.
-              deltaq = hprev.callCabal2nixWithOptions "deltaq" (deltaq.outPath + "/lib/deltaq") "--no-check" { };
+              deltaq = hprev.callCabal2nixWithOptions "deltaq" (
+                inputs.deltaq-src.outPath + "/lib/deltaq"
+              ) "--no-check" { };
               probability-polynomial = hprev.callCabal2nixWithOptions "probability-polynomial" (
-                deltaq.outPath + "/lib/probability-polynomial"
+                inputs.deltaq-src.outPath + "/lib/probability-polynomial"
               ) "--no-check" { };
               # Use a more recent version of `lattices` than is available in the curated Nix package set.
               lattices = hprev.callPackage (
@@ -144,7 +127,7 @@
           overlays = [ overlay ];
         };
 
-        inherit (jupyenv.lib.${system}) mkJupyterlabNew;
+        inherit (inputs.jupyenv.lib.${system}) mkJupyterlabNew;
 
         jupyterlab = mkJupyterlabNew (
           { ... }:
@@ -182,7 +165,7 @@
             #!${pkgs.runtimeShell}
             chmod 0777 tmp
             mkdir -p home/deltaq/examples
-            cp -r ${self}/examples home/deltaq/
+            cp -r ${self}/analysis/deltaq/examples home/deltaq/
           '';
           config = {
             User = "deltaq";
@@ -201,13 +184,15 @@
         };
 
       in
-      rec {
-        packages = { inherit jupyterlab docker; };
-        packages.default = jupyterlab;
-        apps.default.program = "${jupyterlab}/bin/jupyter-lab";
-        apps.default.type = "app";
+      {
+        packages = {
+          inherit jupyterlab docker;
+        };
+
+        apps.jupyterlab = {
+          program = "${jupyterlab}/bin/jupyter-lab";
+          type = "app";
+        };
       }
-
     );
-
 }
