input-output-hk/ouroboros-leios

commit a9a357690fbdbd907253386c669274b6d7d9bfa6

chore: Apply `nix fmt`

post-cip/mempool-sim-web/flake.nix
@@ -7,8 +7,13 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;

commit 62e8a2685456ace797a03a353b2018a920f8f9c6

Fixed flake broken by a9a357690fbdbd907253386c669274b6d7d9bfa6

post-cip/mempool-sim-web/flake.nix
@@ -9,6 +9,7 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       flake-utils,
     }: